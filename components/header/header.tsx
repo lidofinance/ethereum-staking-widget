b8ef82f38de4712526d723f4cdf80ac455fd0945
Anton Shalimov <a.shalimof@yandex.ru>
2021-09-21 00:27:16 +0300

    refactor: order of imports

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,13 +1,13 @@
 import { FC } from 'react';
 import Link from 'next/link';
 import { LidoLogo } from '@lidofinance/lido-logo';
+import Navigation from 'components/navigation';
 import {
   HeaderStyle,
   HeaderLogoStyle,
   HeaderActionsStyle,
 } from './headerStyles';
 import HeaderWallet from './headerWallet';
-import Navigation from 'components/navigation';
 
 const Header: FC = () => (
   <HeaderStyle size="full" forwardedAs="header">
