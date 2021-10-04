53d52fdcb9f81635a70a2031c7954ed78ce9229d
Anton Shalimov <a.shalimof@yandex.ru>
2021-10-05 01:44:46 +0300

    feat: /api/oneinch-rate method #2

--- config/cache.ts
+++ config/cache.ts
@@ -1,11 +1,17 @@
+const duration_1_hour = 1000 * 60 * 60;
+const duration_1_minute = 60 * 1000;
+
 export const CACHE_STETH_APR_KEY = 'cache-steth-apr';
-export const CACHE_STETH_APR_TTL = 1000 * 60 * 60; // 1 hour
+export const CACHE_STETH_APR_TTL = duration_1_hour;
 
 export const CACHE_ETH_APR_KEY = 'cache-eth-apr';
-export const CACHE_ETH_APR_TTL = 1000 * 60 * 60; // 1 hour
+export const CACHE_ETH_APR_TTL = duration_1_hour;
 
 export const CACHE_LIDO_STATS_KEY = 'cache-lido-stats';
-export const CACHE_LIDO_STATS_TTL = 1000 * 60 * 60; // 1 hour
+export const CACHE_LIDO_STATS_TTL = duration_1_hour;
 
 export const CACHE_ETH_PRICE_KEY = 'cache-eth-price';
-export const CACHE_ETH_PRICE_TTL = 60 * 1000; // 1 minute
+export const CACHE_ETH_PRICE_TTL = duration_1_minute;
+
+export const CACHE_ONE_INCH_RATE_KEY = 'oneinch-rate';
+export const CACHE_ONE_INCH_RATE_TTL = duration_1_hour;
