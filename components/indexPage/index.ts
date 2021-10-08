a3eb98f317142dd5a435c96f7aada86dc3c59ca5
Anton Shalimov <a.shalimof@yandex.ru>
2021-10-08 16:12:05 +0300

    refactor: delete unnecessary

--- components/indexPage/index.ts
+++ components/indexPage/index.ts
@@ -1,5 +1,3 @@
 export { default as IndexWallet } from './wallet';
 export { default as LidoStats } from './stats';
 export { default as StakeForm } from './form';
-// TODO: delete referralCard export
-// export { default as ReferralCard } from './referralCard';
