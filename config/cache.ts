e1edaf60e1a8e21316067f1390b6c10ccb8056f9
Anton Shalimov <a.shalimof@yandex.ru>
2021-10-08 12:32:13 +0300

    refactor: code style

--- config/cache.ts
+++ config/cache.ts
@@ -1,17 +1,17 @@
-const duration_1_hour = 1000 * 60 * 60;
-const duration_1_minute = 60 * 1000;
+const duration1Hour = 1000 * 60 * 60;
+const duration1Minute = 1000 * 60;
 
 export const CACHE_STETH_APR_KEY = 'cache-steth-apr';
-export const CACHE_STETH_APR_TTL = duration_1_hour;
+export const CACHE_STETH_APR_TTL = duration1Hour;
 
 export const CACHE_ETH_APR_KEY = 'cache-eth-apr';
-export const CACHE_ETH_APR_TTL = duration_1_hour;
+export const CACHE_ETH_APR_TTL = duration1Hour;
 
 export const CACHE_LIDO_STATS_KEY = 'cache-lido-stats';
-export const CACHE_LIDO_STATS_TTL = duration_1_hour;
+export const CACHE_LIDO_STATS_TTL = duration1Hour;
 
 export const CACHE_ETH_PRICE_KEY = 'cache-eth-price';
-export const CACHE_ETH_PRICE_TTL = duration_1_minute;
+export const CACHE_ETH_PRICE_TTL = duration1Minute;
 
 export const CACHE_ONE_INCH_RATE_KEY = 'oneinch-rate';
-export const CACHE_ONE_INCH_RATE_TTL = duration_1_hour;
+export const CACHE_ONE_INCH_RATE_TTL = duration1Hour;
