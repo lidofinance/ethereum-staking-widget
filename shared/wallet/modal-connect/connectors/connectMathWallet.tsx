632afe15ed14d67ea0b0ad54811b7e2dd5d68aa4
Alexander Khramov <alx.khramov@gmail.com>
2021-11-09 13:54:33 +0300

    refactor: use MathWalletCircle icon from '@lidofinance/icons'

--- shared/wallet/modal-connect/connectors/connectMathWallet.tsx
+++ shared/wallet/modal-connect/connectors/connectMathWallet.tsx
@@ -1,12 +1,17 @@
 import { FC, useCallback } from 'react';
 import { ConnectWalletProps } from './types';
 import ConnectButton from './connectButton';
-import iconUrl from 'assets/icons/mathwallet.svg';
+import {
+  MathWalletCircle,
+  MathWalletCircleInversion,
+} from '@lidofinance/icons';
+import { useThemeToggle } from 'shared/hooks';
 import { useConnectorMathWallet } from '@lido-sdk/web3-react';
 
 const ConnectMathWallet: FC<ConnectWalletProps> = (props) => {
   const { onConnect, ...rest } = props;
   const { connect } = useConnectorMathWallet();
+  const { themeName } = useThemeToggle();
 
   const handleConnect = useCallback(async () => {
     onConnect?.();
@@ -14,7 +19,17 @@ const ConnectMathWallet: FC<ConnectWalletProps> = (props) => {
   }, [onConnect, connect]);
 
   return (
-    <ConnectButton {...rest} iconSrc={iconUrl} onClick={handleConnect}>
+    <ConnectButton
+      {...rest}
+      iconSrcOrReactElement={
+        themeName === 'dark' ? (
+          <MathWalletCircleInversion />
+        ) : (
+          <MathWalletCircle />
+        )
+      }
+      onClick={handleConnect}
+    >
       MathWallet
     </ConnectButton>
   );
