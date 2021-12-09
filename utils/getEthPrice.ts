62bce8d1dacfff6c7cae88bdb052d7ea8afc95ff
Anton Shalimov <a.shalimof@yandex.ru>
2021-12-09 12:55:59 +0300

    feat: api/eth-price with fallbacks

--- utils/getEthPrice.ts
+++ utils/getEthPrice.ts
@@ -8,17 +8,38 @@ import {
 
 export const getEthPrice = async (): Promise<number> => {
   const urls = getRpcJsonUrls(CHAINS.Mainnet);
+  return getEthPriceWithFallbacks(urls, 0);
+};
 
-  const address = getAggregatorAddress(CHAINS.Mainnet);
-  const staticProvider = getStaticRpcBatchProvider(CHAINS.Mainnet, urls[0]);
+const getEthPriceWithFallbacks = async (
+  urls: Array<string>,
+  urlIndex: number,
+): Promise<number> => {
+  // TODO: remove api-key from log
+  console.log('[getEthPrice] Try get via', urls[urlIndex]);
 
-  const contractFactory = getAggregatorContractFactory();
-  const contract = contractFactory.connect(address, staticProvider);
+  try {
+    const address = getAggregatorAddress(CHAINS.Mainnet);
+    const staticProvider = getStaticRpcBatchProvider(
+      CHAINS.Mainnet,
+      urls[urlIndex],
+    );
 
-  const [decimals, latestAnswer] = await Promise.all([
-    contract.decimals(),
-    contract.latestAnswer(),
-  ]);
+    const contractFactory = getAggregatorContractFactory();
+    const contract = contractFactory.connect(address, staticProvider);
 
-  return latestAnswer.toNumber() / 10 ** decimals;
+    const [decimals, latestAnswer] = await Promise.all([
+      contract.decimals(),
+      contract.latestAnswer(),
+    ]);
+
+    return latestAnswer.toNumber() / 10 ** decimals;
+  } catch (error) {
+    if (urlIndex >= urls.length - 1) {
+      console.log('Healthy RPC services are over! Throw error');
+      throw error;
+    }
+  }
+
+  return await getEthPriceWithFallbacks(urls, urlIndex + 1);
 };
