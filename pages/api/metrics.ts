abc05109c0e66268fcf745cb4dda84fe574e3d2b
Anton Shalimov <a.shalimof@yandex.ru>
2021-12-20 16:44:42 +0300

    fix: metrics

--- pages/api/metrics.ts
+++ pages/api/metrics.ts
@@ -1,19 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { collectDefaultMetrics, register } from 'prom-client';
-import { METRICS_PREFIX } from 'config';
-import { collectBuildInfo, collectChainConfig } from 'utilsApi/metrics';
+import { registry } from 'utilsApi/metrics';
 
 type Metrics = (req: NextApiRequest, res: NextApiResponse) => Promise<void>;
 
-collectDefaultMetrics({ prefix: METRICS_PREFIX });
-
 const metrics: Metrics = async (req, res) => {
-  collectBuildInfo();
-  collectChainConfig();
-
-  res.setHeader('Content-type', register.contentType);
-  const allMetrics = await register.metrics();
-  res.send(allMetrics);
+  const collectedMetrics = await registry.metrics();
+  res.send(collectedMetrics);
 };
 
 export default metrics;
--- utilsApi/metrics.ts
+++ utilsApi/metrics.ts
@@ -1,5 +1,4 @@
-// import { Gauge, Histogram, register } from 'prom-client';
-import { Gauge, Histogram } from 'prom-client';
+import { Gauge, Histogram, Registry, collectDefaultMetrics } from 'prom-client';
 import getConfig from 'next/config';
 import { METRICS_PREFIX } from 'config';
 import buildInfoJson from 'build-info.json';
@@ -7,23 +6,17 @@ import buildInfoJson from 'build-info.json';
 const { publicRuntimeConfig } = getConfig();
 const { defaultChain, supportedChains } = publicRuntimeConfig;
 
-// Clear the register to avoid errors on Hot Reload
-// if (process.env.NODE_ENV === 'development') {
-//   register.clear();
-// }
-
 // BUILD_INFO
 const buildInfo = new Gauge({
   name: METRICS_PREFIX + 'build_info',
-  help: 'Build information',
+  help: 'Version, branch and commit of the current build',
   labelNames: ['version', 'commit', 'branch'],
+  registers: [],
 });
 
-export const collectBuildInfo = (): void => {
-  const { version, commit, branch } = buildInfoJson;
+const version = process.env.npm_package_version ?? 'unversioned';
 
-  buildInfo.labels(version, commit, branch).set(1);
-};
+buildInfo.labels(version, buildInfoJson.branch, buildInfoJson.commit).set(1);
 // /BUILD_INFO
 
 // CHAIN CONFIG
@@ -31,11 +24,10 @@ const chainConfig = new Gauge({
   name: METRICS_PREFIX + 'chain_config_info',
   help: 'Default network and supported networks',
   labelNames: ['default_chain', 'supported_chains'],
+  registers: [],
 });
 
-export const collectChainConfig = (): void => {
-  chainConfig.labels(defaultChain, supportedChains).set(1);
-};
+chainConfig.labels(defaultChain, supportedChains).set(1);
 // /CHAIN CONFIG
 
 // RPC RESPONSE
@@ -47,6 +39,7 @@ export const rpcResponseTime = new Histogram({
   help: 'RPC service response time seconds',
   buckets: [0.1, 0.2, 0.3, 0.6, 1, 1.5, 2, 5],
   labelNames: ['provider'],
+  registers: [],
 });
 // /RPC RESPONSE
 
@@ -55,5 +48,17 @@ export const subgraphsResponseTime = new Histogram({
   name: METRICS_PREFIX + 'subgraphs_response',
   help: 'Subgraphs response time seconds',
   buckets: [0.1, 0.2, 0.3, 0.6, 1, 1.5, 2, 5],
+  registers: [],
 });
 // /SUBGRAPHS RESPONSE
+
+export const registry = new Registry();
+
+if (process.env.NODE_ENV === 'production') {
+  registry.registerMetric(buildInfo);
+  registry.registerMetric(chainConfig);
+  registry.registerMetric(rpcResponseTime);
+  registry.registerMetric(subgraphsResponseTime);
+
+  collectDefaultMetrics({ prefix: METRICS_PREFIX, register: registry });
+}
