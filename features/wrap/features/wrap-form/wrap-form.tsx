d754db0d760dbf6d09014d45da16b3fbd2498e4f
Azat Serikov <azatsdev@gmail.com>
2022-04-14 16:46:38 +0600

    fix: remove logs

--- features/wrap/features/wrap-form/wrap-form.tsx
+++ features/wrap/features/wrap-form/wrap-form.tsx
@@ -114,8 +114,6 @@ export const WrapForm: FC = memo(() => {
 
   const approveTxCostInUsd = useTxCostInUsd(approveGasLimit);
   const wrapTxCostInUsd = useTxCostInUsd(wrapGasLimit);
-  console.log({ approveGasLimit, approveTxCostInUsd });
-  console.log({ wrapGasLimit, wrapTxCostInUsd });
 
   const oneWstethConverted = useWstethBySteth(oneSteth);
 
