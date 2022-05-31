e135c62162e5427947049d9da534deb47612915e
kolyasapphire <a@ks.gg>
2022-05-31 21:11:46 +0300

    fix: separate Subgraph chains type

--- utilsApi/getSubgraphUrl.ts
+++ utilsApi/getSubgraphUrl.ts
@@ -1,5 +1,6 @@
 import getConfig from 'next/config';
 import { CHAINS } from '@lido-sdk/constants';
+import { SubgraphChains } from 'types';
 
 const { serverRuntimeConfig } = getConfig();
 export const SUBGRAPH_URL = {
@@ -11,6 +12,6 @@ export const SUBGRAPH_URL = {
   [CHAINS.Kintsugi]: serverRuntimeConfig.subgraphKintsugi,
 } as const;
 
-export const getSubgraphUrl = (chainId: CHAINS): string | undefined => {
+export const getSubgraphUrl = (chainId: SubgraphChains): string | undefined => {
   return SUBGRAPH_URL[chainId];
 };
