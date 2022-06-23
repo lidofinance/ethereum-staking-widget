b54ac69dde64739e5b8768e5c2db875eb8f48756
azat <39704351+mymphe@users.noreply.github.com>
2022-06-23 19:38:09 +0600

    Revert "feat: use 1inch deeplink for Ledger Live"

--- features/home/oneinch-popup/styles.ts
+++ features/home/oneinch-popup/styles.ts
@@ -1,31 +1,7 @@
+import { Button } from '@lidofinance/lido-ui';
 import styled from 'styled-components';
 
-export const ButtonLink = styled.a`
-  display: block;
-  text-decoration: none;
-  text-align: center;
-  line-height: 1em;
-  box-sizing: border-box;
-  margin: 0;
-  border: none;
-  outline: none;
-  white-space: nowrap;
-  overflow: hidden;
-  font-family: inherit;
-  font-weight: 700;
-  font-size: ${({ theme }) => theme.fontSizesMap.xs}px;
-  border-radius: ${({ theme }) => theme.borderRadiusesMap.lg}px;
-  padding: 21px 44px;
-  min-width: 120px;
-  color: ${({ theme }) => theme.colors.primaryContrast};
-  background-color: ${({ theme }) => theme.colors.primary};
-
-  :not(:disabled):hover,
-  :focus-visible {
-    background-color: ${({ theme }) => theme.colors.primaryHover};
-    color: ${({ theme }) => theme.colors.primaryContrast};
-  }
-
+export const ButtonWithMargin = styled(Button)`
   margin: ${({ theme }) => theme.spaceMap.md}px 0;
 `;
 
