65c7f2b04087350df550de7a44b265dd6b5f3333
Alexander Khramov <alx.khramov@gmail.com>
2022-07-21 12:28:47 +0300

    fix: revert of revert 1inch mobile deeplink
    This reverts commit 59f8ef1731b192d30e217be2e080262e4b19dc8f.

--- features/home/oneinch-popup/oneinch-popup.tsx
+++ features/home/oneinch-popup/oneinch-popup.tsx
@@ -9,9 +9,7 @@ import { isDesktop } from 'react-device-detect';
 
 const ONE_INCH_URL = 'https://app.1inch.io/#/1/swap/ETH/steth';
 const LEDGER_LIVE_ONE_INCH_DESKTOP_DEEPLINK = 'ledgerlive://discover/1inch-lld';
-
-// doesn't work for now
-// const LEDGER_LIVE_ONE_INCH_MOBILE_DEEPLINK = 'ledgerlive://discover/1inch-llm';
+const LEDGER_LIVE_ONE_INCH_MOBILE_DEEPLINK = 'ledgerlive://discover/1inch-llm';
 
 export const OneinchPopup: FC<{ modalView: boolean }> = ({ modalView }) => {
   const { isLedgerLive } = useConnectorInfo();
@@ -32,9 +30,11 @@ export const OneinchPopup: FC<{ modalView: boolean }> = ({ modalView }) => {
   let link = ONE_INCH_URL;
   let linkTarget = '_blank';
 
-  const openInLedgerLive = isLedgerLive && isDesktop;
-  if (openInLedgerLive) {
-    link = LEDGER_LIVE_ONE_INCH_DESKTOP_DEEPLINK;
+  if (isLedgerLive) {
+    link = isDesktop
+      ? LEDGER_LIVE_ONE_INCH_DESKTOP_DEEPLINK
+      : LEDGER_LIVE_ONE_INCH_MOBILE_DEEPLINK;
+
     linkTarget = '_self';
   }
 
