64c12d87650fc54aec6f3812332ff76519efbaf9
Nikita Kozlov <nikita.kozlov@p2p.org>
2022-08-04 18:08:28 +0300

    feat: log level is text

---
+++ next-logger.config.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const pino = require('pino'); // It's ok that pino is transit dependency, it's required by next-logger
+
+const logger = (defaultConfig) =>
+  pino({
+    ...defaultConfig,
+    formatters: {
+      level(label, number) {
+        return { level: label };
+      },
+    },
+  });
+
+module.exports = {
+  logger,
+};
