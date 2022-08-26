08f44648ee40ba9d5aeafd6725cdce667686feaa
Andrei <t.fane@mail.ru>
2022-08-26 12:39:06 +0300

    feat: wrap ldo-stats request

--- pages/api/ldo-stats.ts
+++ pages/api/ldo-stats.ts
@@ -1,6 +1,17 @@
 import { Cache } from 'memory-cache';
-import { CACHE_LDO_STATS_KEY, CACHE_LDO_STATS_TTL } from 'config';
-import { getLdoStats, serverErrorHandler } from 'utilsApi';
+import {
+  CACHE_LDO_STATS_KEY,
+  CACHE_LDO_STATS_TTL,
+  CACHE_DEFAULT_HEADERS,
+  CACHE_DEFAULT_ERROR_HEADERS,
+} from 'config';
+import {
+  getLdoStats,
+  wrapRequest,
+  defaultErrorHandler,
+  cacheControl,
+  errorCacheControl,
+} from 'utilsApi';
 import { API } from 'types';
 
 const cache = new Cache<typeof CACHE_LDO_STATS_KEY, unknown>();
@@ -9,20 +20,20 @@ const cache = new Cache<typeof CACHE_LDO_STATS_KEY, unknown>();
 // Returns LDO token information
 // DEPRECATED: In future will be delete!!!
 const ldoStats: API = async (req, res) => {
-  try {
-    const cachedLidoStats = cache.get(CACHE_LDO_STATS_KEY);
+  const cachedLidoStats = cache.get(CACHE_LDO_STATS_KEY);
 
-    if (cachedLidoStats) {
-      res.status(200).json(cachedLidoStats);
-    } else {
-      const ldoStats = await getLdoStats();
-      cache.put(CACHE_LDO_STATS_KEY, { data: ldoStats }, CACHE_LDO_STATS_TTL);
+  if (cachedLidoStats) {
+    res.status(200).json(cachedLidoStats);
+  } else {
+    const ldoStats = await getLdoStats();
+    cache.put(CACHE_LDO_STATS_KEY, { data: ldoStats }, CACHE_LDO_STATS_TTL);
 
-      res.status(200).json({ data: ldoStats });
-    }
-  } catch (error) {
-    serverErrorHandler(error, res);
+    res.status(200).json({ data: ldoStats });
   }
 };
 
-export default ldoStats;
+export default wrapRequest(ldoStats, [
+  cacheControl(CACHE_DEFAULT_HEADERS),
+  errorCacheControl(CACHE_DEFAULT_ERROR_HEADERS),
+  defaultErrorHandler,
+]);
