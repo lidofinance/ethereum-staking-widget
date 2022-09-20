495f08f074ff36d38f06ba55e1eba7c4f376ecae
Anton Shalimov <a.shalimof@yandex.ru>
2022-09-20 14:42:42 +0300

    fix: naming

--- utils/removeOldCookies.ts
+++ utils/removeOldCookies.ts
@@ -8,20 +8,20 @@ const STORAGE_THEME_MANUAL_KEY = 'lido-theme-manual';
 export const removeOldCookiesClientSide = (): void => {
   const topLevelDomain = getTopLevelDomain();
 
-  const cookie_theme_auto = Cookies.get(STORAGE_THEME_AUTO_KEY);
-  const cookie_theme_manual = Cookies.get(STORAGE_THEME_MANUAL_KEY);
+  const old_cookie_theme_auto = Cookies.get(STORAGE_THEME_AUTO_KEY);
+  const old_cookie_theme_manual = Cookies.get(STORAGE_THEME_MANUAL_KEY);
 
   Cookies.remove(STORAGE_THEME_AUTO_KEY);
   Cookies.remove(STORAGE_THEME_MANUAL_KEY);
 
-  if (cookie_theme_auto) {
-    const cookie = `${STORAGE_THEME_AUTO_KEY}=${cookie_theme_auto};expires=${themeCookieExpire};path=/;domain=${topLevelDomain};samesite=None;`;
+  if (old_cookie_theme_auto) {
+    const cookie = `${STORAGE_THEME_AUTO_KEY}=${old_cookie_theme_auto};expires=${themeCookieExpire};path=/;domain=${topLevelDomain};samesite=None;`;
     document.cookie = cookie;
     document.cookie = `${cookie}Secure;`;
   }
 
-  if (cookie_theme_manual) {
-    const cookie = `${STORAGE_THEME_MANUAL_KEY}=${cookie_theme_manual};expires=${themeCookieExpire};path=/;domain=${topLevelDomain};samesite=None;`;
+  if (old_cookie_theme_manual) {
+    const cookie = `${STORAGE_THEME_MANUAL_KEY}=${old_cookie_theme_manual};expires=${themeCookieExpire};path=/;domain=${topLevelDomain};samesite=None;`;
     document.cookie = cookie;
     document.cookie = `${cookie}Secure;`;
   }
