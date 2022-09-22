d67117bd2d92956e6e2795bc11a101f1f757300c
Andrei <t.fane@mail.ru>
2022-09-22 22:02:49 +0300

    fix: change rate limit const

--- config/rateLimit.ts
+++ config/rateLimit.ts
@@ -3,5 +3,5 @@ const { serverRuntimeConfig } = getConfig();
 const { rateLimit, rateLimitTimeFrame } = serverRuntimeConfig;
 
 // requests per RATE_LIMIT_TIME_FRAME
-export const RATE_LIMIT = rateLimit || 60;
+export const RATE_LIMIT = rateLimit || 100;
 export const RATE_LIMIT_TIME_FRAME = rateLimitTimeFrame || 60; // 1 minute
