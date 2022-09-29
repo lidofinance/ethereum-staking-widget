30dcb1894ef19c26f0e0146653a27ac9a20a5996
Andrei <t.fane@mail.ru>
2022-09-29 13:02:38 +0400

    fix: update text and links

--- features/referral/banner/banner.tsx
+++ features/referral/banner/banner.tsx
@@ -9,11 +9,20 @@ export const Banner: FC = () => {
       <BannerHeader>Whitelist mode is on</BannerHeader>
       <p>
         The Lido referral program transitioned to &apos;whitelist mode&apos;
-        starting from 13.09.2021 / 00:00 UTC. Only
+        starting from 13.09.2021 / 00:00 UTC.
+      </p>
+      <br />
+      <p>
+        Only
         <Link href="https://docs.google.com/spreadsheets/d/13JO906tAVoPW9m0F1I39bxB2UIu7E41NSbxIFIzT18I/edit#gid=1322321646">
-          &nbsp;whitelisted referral partners approved by the Lido DAO&nbsp;
+          &nbsp;whitelisted referral&nbsp;
         </Link>
-        are eligible for rewards.
+        partners approved by the Lido DAO are eligible for rewards. To apply to
+        Lido&apos;s whitelist you could here:
+        <Link href="https://research.lido.fi/t/referral-program-whitelisting-ethereum/1039">
+          {' '}
+          https://research.lido.fi/t/referral-program-whitelisting-ethereum/1039
+        </Link>
       </p>
       <br />
       <BannerMainTextStyle>
@@ -22,8 +31,8 @@ export const Banner: FC = () => {
       <br />
       <BannerTextStyle>
         All rewards earned before 13.09.2021 can be claimed via
-        <Link href="https://app.rhino.fi/claim/lido-rewards"> DeversiFi </Link>
-        from 20.09.2021.
+        <Link href="https://app.rhino.fi/claim/lido-rewards"> Rhino.fi </Link>
+        from 20.09.2021
       </BannerTextStyle>
       <br />
       <p>Thank you for participating!</p>
