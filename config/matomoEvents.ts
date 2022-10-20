e57978ec075dd16cb9de1961e6c07ac699e43609
Andrei <t.fane@mail.ru>
2022-10-20 22:57:37 +0400

    fix: add 1icnh event, fix mamoto events page name

--- config/matomoEvents.ts
+++ config/matomoEvents.ts
@@ -6,20 +6,39 @@ const enum EVENTS {
   submitWrap = 'submitWrap',
   submitUnwrap = 'submitUnwrap',
   calculateRewards = 'calculateRewards',
+  oneInchDiscount = 'oneInchDiscount',
 }
 
 export const MATOMO_EVENTS: Record<EVENTS, MatomoEvent> = {
   connectWallet: [
-    'Stake_page',
+    'Ethereum_Stacking_Widget',
     'Push "Connect wallet" button',
     'eth_widget_connect_wallet',
   ],
-  submitStake: ['Stake_page', 'Push submit button', 'eth_widget_stake_eth'],
-  submitWrap: ['Wrap_page', 'Push "Wrap" button', 'eth_widget_wrap_eth'],
-  submitUnwrap: ['Wrap_page', 'Push "Unwrap button"', 'eth_widget_unwrap_eth'],
+  submitStake: [
+    'Ethereum_Stacking_Widget',
+    'Push submit button',
+    'eth_widget_stake_eth',
+  ],
+  submitWrap: [
+    'Ethereum_Stacking_Widget',
+    'Push "Wrap" button',
+    'eth_widget_wrap_eth',
+  ],
+  submitUnwrap: [
+    'Ethereum_Stacking_Widget',
+    'Push "Unwrap button"',
+    'eth_widget_unwrap_eth',
+  ],
+  oneInchDiscount: [
+    'Ethereum_Stacking_Widget',
+    'Push "Get discount" on 1inch template on widget',
+    // TODO: add event name
+    '',
+  ],
   // rewards history
   calculateRewards: [
-    'Rewards_page',
+    'Ethereum_Stacking_Widget',
     'Push calculate reward button" ',
     'eth_widget_calculate reward',
   ],
