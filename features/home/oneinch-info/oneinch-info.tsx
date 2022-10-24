16171a7bd29b95b674573138caddf9d365558d36
Andrei <t.fane@mail.ru>
2022-10-24 23:45:36 +0400

    fix: fix flashing l2 banner

--- features/home/oneinch-info/oneinch-info.tsx
+++ features/home/oneinch-info/oneinch-info.tsx
@@ -13,14 +13,19 @@ import {
 } from './styles';
 import { use1inchLinkProps } from '../hooks';
 
-const ONE_INCH_RATE_LIMIT = 1.004;
+const ONE_INCH_RATE_LIMIT = 1.0001;
 
 export const OneinchInfo: FC = () => {
-  const { data } = useLidoSWR<{ rate: number }>('/api/oneinch-rate');
+  const { data, initialLoading } = useLidoSWR<{ rate: number }>(
+    '/api/oneinch-rate',
+  );
   const rate = (data && data.rate) || 1;
   const discount = (100 - (1 / rate) * 100).toFixed(2);
 
   const linkProps = use1inchLinkProps();
+
+  // for fix flashing banner
+  if (initialLoading) return null;
 
   if (!rate || rate < ONE_INCH_RATE_LIMIT) return <L2Banner />;
 
