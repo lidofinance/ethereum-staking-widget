8a0682d56019840663fe4b4ba6dc6506d67f2d5b
Evgeny Taktarov <je.mouth@gmail.com>
2023-02-17 18:11:39 +0700

    fix: wording, spelling, escaped chars

--- features/home/stake-form/stake-form.tsx
+++ features/home/stake-form/stake-form.tsx
@@ -195,8 +195,8 @@ export const StakeForm: FC = memo(() => {
         <DataTableRow
           title="Reward fee"
           loading={lidoFee.initialLoading}
-          help="Please note: this fee applies to staking rewards/earnings only,
-          and is NOT taken from your staked amount. It is a fee on earnings only."
+          help="Please note: this fee applies to staking rewards only,
+          and is NOT taken from your staked amount."
         >
           {!lidoFee.data ? DATA_UNAVAILABLE : `${lidoFee.data / 100}%`}
         </DataTableRow>
