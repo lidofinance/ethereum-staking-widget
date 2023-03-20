d8ea9af5a8122550e96582b0db63ca96d9037a79
Alexander Khramov <alx.khramov@gmail.com>
2023-03-20 19:55:28 +0300

    feat: config next.js to use ESM, update packages

---
+++ commitlint.config.cjs
@@ -0,0 +1,6 @@
+// This file has .cjs extension and CJS exports because of this issue:
+// https://github.com/conventional-changelog/commitlint/issues/3251
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+};
