8655c751b42514fe546847785add34cc951d51eb
Alexander Khramov <alx.khramov@gmail.com>
2023-03-22 18:41:27 +0300

    feat: config wagmi

--- providers/web3.tsx
+++ providers/web3.tsx
@@ -1,15 +1,37 @@
 import { FC } from 'react';
-import { ProviderWeb3 } from '@reef-knot/web3-react';
-import { backendRPC, dynamics } from 'config';
+import { ProviderWeb3 } from 'reef-knot/web3-react';
+import { backendRPC, getBackendRPCPath, dynamics } from 'config';
+import { WagmiConfig, createClient, configureChains } from 'wagmi';
+import { mainnet, goerli } from 'wagmi/chains';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+
+const { provider, webSocketProvider } = configureChains(
+  [mainnet, goerli],
+  [
+    jsonRpcProvider({
+      rpc: (chain) => ({
+        http: getBackendRPCPath(chain.id),
+      }),
+    }),
+  ],
+);
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+});
 
 const Web3Provider: FC = ({ children }) => (
-  <ProviderWeb3
-    defaultChainId={dynamics.defaultChain}
-    supportedChainIds={dynamics.supportedChains}
-    rpc={backendRPC}
-  >
-    {children}
-  </ProviderWeb3>
+  <WagmiConfig client={client}>
+    <ProviderWeb3
+      defaultChainId={dynamics.defaultChain}
+      supportedChainIds={dynamics.supportedChains}
+      rpc={backendRPC}
+    >
+      {children}
+    </ProviderWeb3>
+  </WagmiConfig>
 );
 
 export default Web3Provider;
