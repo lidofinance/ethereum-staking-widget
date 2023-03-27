f102c4ecc0e362bd1e289d7e260158ab2c96a072
Andrei <t.fane@mail.ru>
2023-03-27 19:07:17 +0700

    fix: fix not connected wallet in header

--- shared/components/header/components/header-wallet.tsx
+++ shared/components/header/components/header-wallet.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { useWeb3 } from '@reef-knot/web3-react';
+import { useSDK } from '@lido-sdk/react';
 import { CHAINS } from '@lido-sdk/constants';
 import { Button, Connect } from 'shared/wallet';
 import { HeaderWalletChainStyle, DotStyle } from '../styles';
@@ -8,9 +9,8 @@ import { ThemeToggler } from '@lidofinance/lido-ui';
 import { getChainColor } from 'customSdk/chains';
 
 const HeaderWallet: FC = () => {
-  const { active, chainId } = useWeb3();
-
-  if (!chainId) return null;
+  const { active } = useWeb3();
+  const { chainId } = useSDK();
 
   const chainName = CHAINS[chainId];
   const testNet = chainId !== CHAINS.Mainnet;
