b3f4b440a984ed7ea23f7a0a56b9f299e0701802
Andrei <t.fane@mail.ru>
2023-04-05 19:53:25 +0700

    feat: add random texts

--- features/withdrawals/hooks/useInputTvlValidate.tsx
+++ features/withdrawals/hooks/useInputTvlValidate.tsx
@@ -7,6 +7,28 @@ import omitBy from 'lodash/omitBy';
 import isEmpty from 'lodash/isEmpty';
 
 import { shortenTokenValue } from 'utils';
+import { useMemo } from 'react';
+
+const texts: Record<string, (amount: string) => string> = {
+  '1': (amount) =>
+    `That's about ${amount} more than you've got, are you sure you're in the right wallet?`,
+  '2': (amount) =>
+    `That's about ${amount} more than you've got, would suggest you stake more first!`,
+  '3': () =>
+    (
+      <>
+        Didn&apos;t realize you&apos;re a &#x1F40B;, did you leave your stETH in
+        your other wallet? No worries, just stake some more!
+      </> // TODO: fix this
+    ) as unknown as string,
+  '4': () => 'Hey Justin Sun, the "stake" button is this way ^',
+};
+
+const getText = () => {
+  const keys = Object.keys(texts);
+  const randomKey = keys[Math.floor(Math.random() * keys.length)];
+  return texts[randomKey];
+};
 
 export const useInputTvlValidate = (inputValue: string) => {
   const stethTotalSupply = useSTETHTotalSupply();
@@ -20,15 +42,12 @@ export const useInputTvlValidate = (inputValue: string) => {
       parseEther(inputValue || '0').sub(stethTotalSupply.data)) ||
     BigNumber.from(0);
 
+  const text = useMemo(() => getText(), []);
   const tvlMessage =
     stethTotalSupply.data &&
-    parseEther(inputValue || '0').gt(stethTotalSupply.data) ? (
-      <>
-        For this request you are missing just{' '}
-        {shortenTokenValue(Number(Number(formatEther(diff))))} stETH. Wanna
-        stake more?
-      </>
-    ) : undefined;
+    parseEther(inputValue || '0').gt(stethTotalSupply.data)
+      ? text(shortenTokenValue(Number(Number(formatEther(diff)))))
+      : undefined;
 
   const stakePath = `/${
     searchParam
--- features/withdrawals/request/form/styles.ts
+++ features/withdrawals/request/form/styles.ts
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
 import { InputGroup } from '@lidofinance/lido-ui';
 
-export const InputGroupStyled = styled(InputGroup)`
+export const InputGroupStyled = styled(InputGroup)<{ success?: string }>`
   margin-bottom: ${({ theme }) => theme.spaceMap.md}px;
   z-index: 2;
+
+  span:nth-of-type(2) {
+    white-space: ${({ success }) => success && 'unset'};
+  }
 `;
 
 export const ButtonLinkWrap = styled.a`
