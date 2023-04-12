f9157afc6022087e45830bc29b39ac99db8e4da6
Dmitrii Podlesnyi <rezed93@gmail.com>
2023-04-12 17:22:04 +0700

    fix: show add nft button only for wallets that supports this action

--- features/withdrawals/request/tx-modal/nft-banner.tsx
+++ features/withdrawals/request/tx-modal/nft-banner.tsx
@@ -22,20 +22,32 @@ export const NFTBanner = () => {
 
         <TextWrapper>
           <TitleStyled>
-            Add this NFT to your wallet to monitor status of your request.
+            {addNft ? (
+              <>
+                Add this NFT to your wallet to monitor status
+                of&nbsp;your&nbsp;request
+              </>
+            ) : (
+              <>
+                Add NFT to your wallet to monitor status
+                of&nbsp;your&nbsp;request.
+              </>
+            )}
           </TitleStyled>
         </TextWrapper>
-        <ButtonWrapperStyled>
-          <ButtonIcon
-            icon={<Plus />}
-            fullwidth
-            size="sm"
-            variant="filled"
-            onClick={addNft}
-          >
-            Add NFT to wallet
-          </ButtonIcon>
-        </ButtonWrapperStyled>
+        {addNft && (
+          <ButtonWrapperStyled>
+            <ButtonIcon
+              icon={<Plus />}
+              fullwidth
+              size="sm"
+              variant="filled"
+              onClick={addNft}
+            >
+              Add NFT to wallet
+            </ButtonIcon>
+          </ButtonWrapperStyled>
+        )}
         <DescriptionStyled>For withdrawals participants only</DescriptionStyled>
       </ContentStyled>
     </NFTBunner>
