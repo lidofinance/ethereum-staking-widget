6f9ba39cb0fa7fc9d2d8b2ab7b2662889913d25f
Dmitrii Podlesnyi <rezed93@gmail.com>
2023-05-04 17:24:10 +0700

    chore: remove turbo info box

--- features/withdrawals/request/form/request-form.tsx
+++ features/withdrawals/request/form/request-form.tsx
@@ -2,20 +2,18 @@ import React from 'react';
 import { Block } from '@lidofinance/lido-ui';
 
 import { Form } from './form';
-import { Info } from './info';
 import { BunkerInfo } from './bunker-info';
 import { PausedInfo } from './paused-info';
 import { useWithdrawalsBaseData } from 'features/withdrawals/hooks';
 
 export const RequestForm = () => {
   const wqBaseData = useWithdrawalsBaseData();
-  const { isBunker, isPaused, isTurbo } = wqBaseData.data ?? {};
+  const { isBunker, isPaused } = wqBaseData.data ?? {};
 
   return (
     <Block>
       {isPaused && <PausedInfo />}
       {isBunker && <BunkerInfo />}
-      {isTurbo && <Info />}
       <Form />
     </Block>
   );
