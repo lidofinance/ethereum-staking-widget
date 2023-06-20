bf8a8b3b3944d3f2fb8e5f43adee25f334b7787b
Alexander Khramov <alx.khramov@gmail.com>
2023-06-20 20:08:42 +0300

    fix: try to make rpc paths to be absolute for WC v2

--- config/rpc.ts
+++ config/rpc.ts
@@ -5,7 +5,8 @@ const { serverRuntimeConfig } = getConfig();
 const { basePath } = serverRuntimeConfig;
 
 export const getBackendRPCPath = (chainId: string | number): string => {
-  return `${basePath ?? ''}/api/rpc?chainId=${chainId}`;
+  const BASE_URL = typeof window === 'undefined' ? '' : window.location.origin;
+  return `${basePath ?? BASE_URL}/api/rpc?chainId=${chainId}`;
 };
 
 export const backendRPC = {
