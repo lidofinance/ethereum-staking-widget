1b59d79bf1e0e1ab8e9869784c1f5ed29171ed77
Alexander Khramov <alx.khramov@gmail.com>
2023-06-22 13:55:54 +0300

    fix: rm env basePath from rpc path
    Attempt to solve an issue with WCv1 rpc path on preview stands.

--- config/rpc.ts
+++ config/rpc.ts
@@ -1,12 +1,8 @@
-import getConfig from 'next/config';
 import { CHAINS } from 'utils/chains';
-
-const { serverRuntimeConfig } = getConfig();
-const { basePath } = serverRuntimeConfig;
 
 export const getBackendRPCPath = (chainId: string | number): string => {
   const BASE_URL = typeof window === 'undefined' ? '' : window.location.origin;
-  return `${basePath ?? BASE_URL}/api/rpc?chainId=${chainId}`;
+  return `${BASE_URL}/api/rpc?chainId=${chainId}`;
 };
 
 export const backendRPC = {
