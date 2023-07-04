c951d9a1608a374a56e9b2071a2b2e686a8545b9
Evgeny Taktarov <je.mouth@gmail.com>
2023-07-04 16:13:32 +0700

    chore: refactor useSafeQueryString

--- shared/hooks/useSafeQueryString.ts
+++ shared/hooks/useSafeQueryString.ts
@@ -6,19 +6,10 @@ export const useSafeQueryString = (extraParams?: Record<string, string>) => {
 
   return useMemo(() => {
     const queryParams = new URLSearchParams();
-    if (ref && typeof ref === 'string') {
-      queryParams.append('ref', ref);
-    }
-    if (embed && typeof embed === 'string') {
-      queryParams.append('embed', embed);
-    }
-    if (app && typeof app === 'string') {
-      queryParams.append('app', app);
-    }
-
-    if (extraParams) {
-      Object.entries(extraParams).forEach(([k, v]) => queryParams.append(k, v));
-    }
+    // mix required and extra params
+    Object.entries({ ...{ ref, embed, app }, ...(extraParams ?? {}) }).forEach(
+      ([k, v]) => v && typeof v === 'string' && queryParams.append(k, v),
+    );
     const qs = queryParams.toString();
     return qs ? '?' + qs : '';
   }, [ref, embed, app, extraParams]);
