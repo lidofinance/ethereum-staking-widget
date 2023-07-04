6f5828e17c04d3d85d18215fb8cd1f6b86e13304
Evgeny Taktarov <Jeday@users.noreply.github.com>
2023-07-04 21:16:53 +0700

    feat: enable withdrawals in ledger live with limit (#657)

--- features/withdrawals/withdrawals-constants/index.ts
+++ features/withdrawals/withdrawals-constants/index.ts
@@ -1,4 +1,5 @@
 // max requests count for one tx
 export const MAX_REQUESTS_COUNT = 256;
+export const MAX_REQUESTS_COUNT_LEDGER_LIMIT = 2;
 export const DEFAULT_CLAIM_REQUEST_SELECTED = 80;
 export const MAX_SHOWN_REQUEST_PER_TYPE = 1024;
