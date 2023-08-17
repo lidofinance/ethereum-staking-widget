94438885ea4449bbec2d40f4b1dd02a2e4f05c64
Evgeny Taktarov <je.mouth@gmail.com>
2023-08-17 20:39:05 +0700

    fix: allow csp endpoint to parse strings

--- pages/api/csp-report.ts
+++ pages/api/csp-report.ts
@@ -3,9 +3,17 @@ import { defaultErrorHandler, rateLimit } from 'utilsApi';
 import { API } from 'types';
 
 const cspReport: API = async (req, res) => {
+  let violation = {};
+
+  if (typeof req.body == 'object') {
+    violation = req.body;
+  } else if (typeof req.body === 'string') {
+    violation = JSON.parse(req.body);
+  }
+
   console.warn({
     type: 'CSP Violation',
-    ...req.body,
+    ...violation,
   });
 
   res.status(200).send({ status: 'ok' });
