08e5ac0ee6f0319ea6fad46926ce49e02a4129df
Dmitrii Podlesnyi <rezed93@gmail.com>
2023-08-22 16:26:35 +0400

    chore: wrap form amount input finishing

--- features/wrap/features/wrap-form/controls/amount-input.tsx
+++ features/wrap/features/wrap-form/controls/amount-input.tsx
@@ -2,14 +2,11 @@ import { useController, useWatch } from 'react-hook-form';
 import { useWrapFormData, WrapFormInputType } from '../../wrap-form-context';
 
 import { InputAmount } from 'shared/forms/components/input-amount';
-// import { InputDecoratorLocked } from 'shared/forms/components/input-decorator-locked';
-// import { InputDecoratorMaxButton } from 'shared/forms/components/input-decorator-max-button';
-// import { InputWrapper } from 'features/wrap/styles';
 
 import { getTokenDisplayName } from 'utils/getTokenDisplayName';
 
 export const AmountInput = () => {
-  const { maxAmount } = useWrapFormData();
+  const { maxAmount, isApprovalNeededBeforeWrap } = useWrapFormData();
   const token = useWatch<WrapFormInputType, 'token'>({ name: 'token' });
   const {
     field,
@@ -21,7 +18,7 @@ export const AmountInput = () => {
       fullwidth
       data-testid="wrapInput"
       error={error?.type === 'validate'}
-      // isLocked={isTokenLocked}
+      isLocked={isApprovalNeededBeforeWrap}
       maxValue={maxAmount}
       label={`${getTokenDisplayName(token)} amount`}
       {...field}
