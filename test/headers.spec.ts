b379fa06d49ed36d989c6564238f35f70e93c000
Evgeny Taktarov <je.mouth@gmail.com>
2023-09-13 17:49:57 +0800

    fix: disable file test on stands

--- test/headers.spec.ts
+++ test/headers.spec.ts
@@ -6,6 +6,7 @@ import {
   CACHE_CONTROL_PAGES,
   CACHE_CONTROL_VALUE,
 } from 'next.config.mjs';
+import { CONFIG } from './config.js';
 
 // case for only wildcard in config
 const configPages = CACHE_CONTROL_PAGES;
@@ -13,6 +14,7 @@ configPages[CACHE_CONTROL_PAGES.indexOf('/favicon:size*')] = '/favicon.ico';
 
 test.describe('Page Headers', () => {
   test('Config should have all static pages', () => {
+    test.skip(!!CONFIG.STAND_TYPE, 'We cannot access files on stands');
     const pageRoutes = getAllPagesRoutes();
     pageRoutes.forEach((foundPage) =>
       expect(CACHE_CONTROL_PAGES.includes(foundPage)).toBe(true),
