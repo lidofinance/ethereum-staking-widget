00bd0f6cfe6726aedf1a31917c18ebf7c9078f43
Anton Shalimov <a.shalimof@yandex.ru>
2023-09-21 12:35:10 +0300

    feat: use FaqAccordion from `@lidofinance/ui-faq` on wrap and unwrap pages

--- features/wsteth/shared/wrap-faq/wrap-faq.tsx
+++ features/wsteth/shared/wrap-faq/wrap-faq.tsx
@@ -1,28 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import { trackEvent } from '@lidofinance/analytics-matomo';
+import { FaqAccordion, getFAQ, PageFAQ } from '@lidofinance/ui-faq';
+import { dynamics } from 'config';
 import { Section } from 'shared/components';
-import { useMatomoEventHandle } from 'shared/hooks';
-
-import {
-  WhatIsWsteth,
-  HowCanIGetWsteth,
-  HowCanIUseWsteth,
-  DoIGetMyStakingRewards,
-  DoINeedToClaimMyStakingRewards,
-  HowCouldIUnwrapWstethToSteth,
-  DoINeedToUnwrapMyWsteth,
-} from './list';
 
 export const WrapFaq = () => {
-  const onClickHandler = useMatomoEventHandle();
+  const [foundPage, setFoundPage] = useState<PageFAQ | undefined>(undefined);
+
+  useEffect(() => {
+    void (async () => {
+      try {
+        const pageIdentification = 'wrapOrUnwrap';
+        const pages = await getFAQ(dynamics.faqContentUrl);
+
+        setFoundPage(
+          pages.find(
+            (page: PageFAQ) => page['identification'] === pageIdentification,
+          ),
+        );
+
+        return () => {};
+      } catch {
+        // noop
+      }
+    })();
+  }, []);
 
   return (
-    <Section title="FAQ" onClick={onClickHandler}>
-      <WhatIsWsteth />
-      <HowCanIGetWsteth />
-      <HowCanIUseWsteth />
-      <DoIGetMyStakingRewards />
-      <DoINeedToClaimMyStakingRewards />
-      <HowCouldIUnwrapWstethToSteth />
-      <DoINeedToUnwrapMyWsteth />
+    <Section title="FAQ">
+      <FaqAccordion
+        faqList={foundPage?.faq}
+        onLinkClick={({ questionId, question, linkContent }) => {
+          const actionEvent = `Push «${linkContent}» in FAQ ${question} on stake widget`;
+          // Make event like `<project_name>_faq_<page_id>_<question_id>_<link_content>`
+          const nameEvent = `eth_widget_faq_wrapOrUnwrap_${questionId}_${linkContent}`;
+          trackEvent('Ethereum_Staking_Widget', actionEvent, nameEvent);
+        }}
+      />
     </Section>
   );
 };
