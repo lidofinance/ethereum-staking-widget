c49803b0e11f4e0fa1c274139ff33b400e4d1e89
Anton Shalimov <a.shalimof@yandex.ru>
2023-09-22 20:08:08 +0300

    refactor: added `PropsWithChildren` to components with children

--- features/withdrawals/request/form/controls/input-group-request.tsx
+++ features/withdrawals/request/form/controls/input-group-request.tsx
@@ -1,9 +1,10 @@
+import { FC, PropsWithChildren } from 'react';
 import { useTvlMessage } from 'features/withdrawals/hooks';
 import { useFormState } from 'react-hook-form';
 import { InputGroupHookForm } from 'shared/hook-form/controls/input-group-hook-form';
 import { RequestFormInputType } from '../../request-form-context';
 
-export const InputGroupRequest: React.FC = ({ children }) => {
+export const InputGroupRequest: FC<PropsWithChildren> = ({ children }) => {
   const {
     errors: { amount: amountError },
   } = useFormState<RequestFormInputType>({ name: 'amount' });
