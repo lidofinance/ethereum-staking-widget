a3abe586260ad5ef852848fb792391fd1b987dea
Anton Shalimov <a.shalimof@yandex.ru>
2023-09-29 13:34:20 +0300

    feat(env-dynamics): add ipfsMode

--- env-dynamics.mjs
+++ env-dynamics.mjs
@@ -32,3 +32,6 @@ export const ethAPIBasePath = process.env.ETH_API_BASE_PATH;
 export const wqAPIBasePath = process.env.WQ_API_BASE_PATH;
 /** @type string */
 export const walletconnectProjectId = process.env.WALLETCONNECT_PROJECT_ID;
+
+/** @type boolean */
+export const ipfsMode = toBoolean(process.env.IPFS_MODE);
