1b9b7703ac356ea156345e75ec9db28a751f87c9
Anton Shalimov <a.shalimof@yandex.ru>
2023-10-02 20:23:25 +0300

    feat(Header NavLink): mark active NavLink in IPFS and regular mode

--- shared/hooks/use-router-path.ts
+++ shared/hooks/use-router-path.ts
@@ -10,5 +10,16 @@ export const useRouterPath = () => {
     return location.hash.replace('#', '') || '/';
   }
 
-  return router.pathname;
+  // We can't' use `router.pathname` and `router.route` 'cause it's a mapping with file structure
+  // example:
+  // - /wrap                  --->  /wrap/[[...mode]]
+  // - /withdrawals/request/  --->  /withdrawals/[mode]
+  // also we need to remove last character because `router.asPath` contain `/` as last character
+  // example:
+  // - /wrap                  ---> - /wrap/
+  // - <empty>                ---> - /
+  if (router.asPath.length > 1 && router.asPath.slice(-1) === '/')
+    return router.asPath.slice(0, -1);
+  // Fix for index page - '/'
+  else return router.asPath;
 };
