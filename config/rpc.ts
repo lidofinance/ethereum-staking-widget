5d2f7431b153fe83ec493708c677437732606017
Evgeny Taktarov <je.mouth@gmail.com>
2023-10-05 16:41:36 +0700

    feat: add holesky support

--- config/rpc.ts
+++ config/rpc.ts
@@ -8,4 +8,5 @@ export const getBackendRPCPath = (chainId: string | number): string => {
 export const backendRPC = {
   [CHAINS.Mainnet]: getBackendRPCPath(CHAINS.Mainnet),
   [CHAINS.Goerli]: getBackendRPCPath(CHAINS.Goerli),
+  [CHAINS.Holesky]: getBackendRPCPath(CHAINS.Holesky),
 };
--- utils/getNFTUrl.ts
+++ utils/getNFTUrl.ts
@@ -7,6 +7,8 @@ export const NFT_URL_PREFIX_BY_NETWORK: {
     `https://etherscan.io/nft/${contract}/${nftId}`,
   [CHAINS.Goerli]: (nftId, contract) =>
     `https://goerli.etherscan.io/nft/${contract}/${nftId}`,
+  [CHAINS.Holesky]: (nftId, contract) =>
+    `https://holesky.etherscan.io/nft/${contract}/${nftId}`,
 };
 
 export const getNFTUrl = (tokenId: string, chainId?: CHAINS) => {
