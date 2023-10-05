ae9dbe1c98969e078e51a3ac4bd117d3b523db07
Anton Shalimov <a.shalimof@yandex.ru>
2023-10-05 11:45:29 +0300

    feat: settings page

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,8 +1,18 @@
 import { FC } from 'react';
 import { Layout } from 'shared/components';
+import {
+  SettingsForm,
+  SettingsFormWrap,
+} from 'features/settings/settings-form';
 
 const Referral: FC = () => {
-  return <Layout title="Settings">TODO</Layout>;
+  return (
+    <Layout title="Settings">
+      <SettingsFormWrap>
+        <SettingsForm />
+      </SettingsFormWrap>
+    </Layout>
+  );
 };
 
 export default Referral;
