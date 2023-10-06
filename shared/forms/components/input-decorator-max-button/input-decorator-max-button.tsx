4383a5e6b67a2e298f1d46705af9c2ae5a2d75f0
Anton Shalimov <a.shalimof@yandex.ru>
2023-10-06 16:18:34 +0300

    feat(max button in withdrawals request form): send a matomo event by click

--- shared/forms/components/input-decorator-max-button/input-decorator-max-button.tsx
+++ shared/forms/components/input-decorator-max-button/input-decorator-max-button.tsx
@@ -1,19 +1,42 @@
+import { MouseEventHandler, MouseEvent, useRef } from 'react';
+
+import {
+  MATOMO_CLICK_EVENTS_TYPES,
+  trackMatomoEvent,
+} from 'config/trackMatomoEvent';
+import { useRequestFormData } from 'features/withdrawals/request/request-form-context';
+
 import { MaxButton } from './styled';
 
 type InputDecoratorMaxButtonProps = {
   disabled?: boolean;
-  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 };
 
 export const InputDecoratorMaxButton = ({
   disabled,
   onClick,
 }: InputDecoratorMaxButtonProps) => {
+  const thisComponentInsideWithdrawalsRequestFormRef = useRef(false);
+
+  try {
+    void useRequestFormData();
+    thisComponentInsideWithdrawalsRequestFormRef.current = true;
+  } catch {
+    /* Not error: it's not withdrawals request form */
+  }
+
   return (
     <MaxButton
       size="xxs"
       variant="translucent"
-      onClick={onClick}
+      onClick={(event: MouseEvent<HTMLButtonElement>) => {
+        onClick?.(event);
+
+        if (thisComponentInsideWithdrawalsRequestFormRef.current) {
+          trackMatomoEvent(MATOMO_CLICK_EVENTS_TYPES.withdrawalMaxInput);
+        }
+      }}
       disabled={disabled}
       data-testid="maxBtn"
     >
