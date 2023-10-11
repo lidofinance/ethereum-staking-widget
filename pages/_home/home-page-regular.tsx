f00b00f832cd3779307f7dcc55a7bae338bb36b5
Anton Shalimov <a.shalimof@yandex.ru>
2023-10-11 20:39:14 +0300

    feat: useWeb3Key on HomePageRegular

--- pages/_home/home-page-regular.tsx
+++ pages/_home/home-page-regular.tsx
@@ -3,25 +3,30 @@ import Head from 'next/head';
 import { Wallet, StakeForm, LidoStats, StakeFaq } from 'features/home';
 import { Layout } from 'shared/components';
 import NoSSRWrapper from 'shared/components/no-ssr-wrapper';
+import { useWeb3Key } from 'shared/hooks/useWeb3Key';
 
-const HomePageRegular: FC = () => (
-  <>
-    <Layout
-      title="Stake Ether"
-      subtitle="Stake ETH and receive stETH while staking."
-    >
-      <Head>
-        <title>Stake with Lido | Lido</title>
-      </Head>
+const HomePageRegular: FC = () => {
+  const key = useWeb3Key();
 
-      <NoSSRWrapper>
-        <Wallet />
-        <StakeForm />
-      </NoSSRWrapper>
-      <LidoStats />
-      <StakeFaq />
-    </Layout>
-  </>
-);
+  return (
+    <>
+      <Layout
+        title="Stake Ether"
+        subtitle="Stake ETH and receive stETH while staking."
+      >
+        <Head>
+          <title>Stake with Lido | Lido</title>
+        </Head>
+
+        <NoSSRWrapper>
+          <Wallet key={'wallet' + key} />
+          <StakeForm key={'form' + key} />
+        </NoSSRWrapper>
+        <LidoStats />
+        <StakeFaq />
+      </Layout>
+    </>
+  );
+};
 
 export default HomePageRegular;
