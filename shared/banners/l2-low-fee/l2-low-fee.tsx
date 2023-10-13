ae8608ba0ee30582411cb7b1ee59aa9f84109235
Andrei <t.fane@mail.ru>
2023-10-13 14:36:47 +0700

    feat: add l2 low fee banner to stake/wrap modals

--- shared/banners/l2-low-fee/l2-low-fee.tsx
+++ shared/banners/l2-low-fee/l2-low-fee.tsx
@@ -1,32 +1,16 @@
-import { FC, useCallback } from 'react';
-import { ThemeProvider, themeDark } from '@lidofinance/lido-ui';
+import { useCallback } from 'react';
 import { trackEvent } from '@lidofinance/analytics-matomo';
 import { MATOMO_CLICK_EVENTS } from 'config';
 
-import {
-  Wrapper,
-  L2Icons,
-  TextWrap,
-  ButtonWrap,
-  ButtonLinkWrap,
-  ButtonStyle,
-  ContentWrap,
-  TextHeader,
-} from './styles';
-
-const LINK = 'https://lido.fi/lido-on-l2';
+import { L2Banner, L2_DISCOVERY_LINK } from '../l2-banner';
 
 type L2LowFeeProps = {
   token: 'stETH' | 'wstETH';
 };
 
-export const L2LowFee: FC<L2LowFeeProps> = ({ token }) => {
-  const linkProps = {
-    href: LINK,
-    target: '_blank',
-    rel: 'noopener noreferrer',
-  };
+export const L2LowFee: React.FC<L2LowFeeProps> = ({ token }) => {
   const isStETH = token === 'stETH';
+
   const linkClickHandler = useCallback(() => {
     const event = isStETH
       ? MATOMO_CLICK_EVENTS.l2LowFeeStake
@@ -35,27 +19,18 @@ export const L2LowFee: FC<L2LowFeeProps> = ({ token }) => {
   }, [isStETH]);
 
   const text = isStETH
-    ? 'Learn about Lido on L2 opportunities on Arbitrum, Optimism, and Polygon PoS to enjoy reduced gas fees in DeFi'
-    : 'Bridge wstETH to Arbitrum, Optimism and Polygon PoS to enjoy low gas fees and enhanced opportunities in DeFi';
+    ? 'Learn about Lido on L2 opportunities on Arbitrum, Optimism, and Base to enjoy reduced gas fees in DeFi'
+    : 'Bridge wstETH to Arbitrum, Optimism and Base to enjoy low gas fees and enhanced opportunities in DeFi';
 
   return (
-    <Wrapper data-testid="l2LowFeeBanner">
-      <ThemeProvider theme={themeDark}>
-        <ContentWrap>
-          <TextWrap>
-            <TextHeader>Unlock Low-Fee transactions on L2</TextHeader>
-            {text}
-          </TextWrap>
-          <ButtonWrap>
-            <ButtonLinkWrap {...linkProps} onClick={linkClickHandler}>
-              <ButtonStyle data-testid="l2LowFee" size="sm" color="secondary">
-                Learn more
-              </ButtonStyle>
-            </ButtonLinkWrap>
-          </ButtonWrap>
-        </ContentWrap>
-        <L2Icons />
-      </ThemeProvider>
-    </Wrapper>
+    <L2Banner
+      title="Unlock Low-Fee transactions on L2"
+      text={text}
+      buttonText="Learn more"
+      buttonHref={L2_DISCOVERY_LINK}
+      testidWrap="l2LowFeeBanner"
+      testidButton="l2LowFee"
+      onClickButton={linkClickHandler}
+    />
   );
 };
