8ecc43eb05338943bd88f662345b4686b1dcb854
Evgeny Taktarov <je.mouth@gmail.com>
2023-10-20 17:10:30 +0700

    fix: export

--- config/index.ts
+++ config/index.ts
@@ -8,7 +8,6 @@ export * from './estimate';
 export * from './locale';
 export * from './metrics';
 export * from './rpc';
-export * from './steth';
 export * from './storage';
 export * from './text';
 export * from './tx';
