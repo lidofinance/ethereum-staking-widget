727dd6286e8805497ced47b70fd3d0dffcdfd2d7
Anton Shalimov <a.shalimof@yandex.ru>
2023-11-07 15:53:56 +0300

    feat: support href + query string in link IPFS

--- shared/hooks/use-prefixed-history.ts
+++ shared/hooks/use-prefixed-history.ts
@@ -7,8 +7,13 @@ export const usePrefixedPush = () => {
   const router = useRouter();
   type Args = Parameters<typeof router.push>;
   return useCallback(
-    (url: string, a1?: Args[1], a2?: Args[2]) => {
-      return router.push(prefixUrl(url), a1, a2);
+    (
+      url: string,
+      query?: Record<string, string>,
+      a1?: Args[1],
+      a2?: Args[2],
+    ) => {
+      return router.push(prefixUrl(url, query), a1, a2);
     },
     [router],
   );
@@ -18,8 +23,13 @@ export const usePrefixedReplace = () => {
   const router = useRouter();
   type Args = Parameters<typeof router.replace>;
   return useCallback(
-    (url: string, a1?: Args[1], a2?: Args[2]) => {
-      return router.replace(prefixUrl(url), a1, a2);
+    (
+      url: string,
+      query?: Record<string, string>,
+      a1?: Args[1],
+      a2?: Args[2],
+    ) => {
+      return router.replace(prefixUrl(url, query), a1, a2);
     },
     [router],
   );
--- utils/get-ipfs-base-path.ts
+++ utils/get-ipfs-base-path.ts
@@ -1,5 +1,6 @@
 import memoize from 'lodash/memoize';
 import { dynamics } from 'config';
+import { encodeURLQuery } from './encodeURLQuery';
 
 export const getIpfsBasePath = memoize(() => {
   let basePath = document.querySelector('base')?.href || '/';
@@ -7,7 +8,10 @@ export const getIpfsBasePath = memoize(() => {
   return basePath;
 });
 
-export const prefixUrl = (url: string) => {
-  if (dynamics.ipfsMode) return `${getIpfsBasePath()}#${url}`;
+export const prefixUrl = (url: string, query?: Record<string, string>) => {
+  const queryString =
+    query && Object.keys(query).length > 0 ? `?${encodeURLQuery(query)}` : '';
+
+  if (dynamics.ipfsMode) return `${getIpfsBasePath()}${queryString}#${url}`;
   return url;
 };
