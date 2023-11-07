bb9b009861fea4e8c95471fb7881b94278e5e106
Anton Shalimov <a.shalimof@yandex.ru>
2023-11-07 19:13:47 +0300

    refactor: move home pages to features

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { dynamics } from 'config';
 
-import HomePageRegular from './_home/home-page-regular';
-import HomePageIpfs from './_home/home-page-ipfs';
+import HomePageRegular from 'features/home/home-page-regular';
+import HomePageIpfs from 'features/ipfs/home-page-ipfs';
 
 export default dynamics.ipfsMode ? HomePageIpfs : HomePageRegular;
