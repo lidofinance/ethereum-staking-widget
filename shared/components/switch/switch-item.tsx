03a7734c7138a3bb01ccdb43795a9504f9880d5e
Anton Shalimov <a.shalimof@yandex.ru>
2023-11-08 12:17:51 +0300

    refactor: check active in switch item component and add useCompareWithRouterPath hook

--- shared/components/switch/switch-item.tsx
+++ shared/components/switch/switch-item.tsx
@@ -1,6 +1,4 @@
-import { useRouter } from 'next/router';
-
-import { dynamics } from 'config';
+import { useCompareWithRouterPath } from 'shared/hooks/use-compare-with-router-path';
 
 import { SwitchItemStyled } from './styles';
 import { SwitchItemComponent } from './types';
@@ -8,19 +6,7 @@ import { LocalLink } from '../local-link';
 
 export const SwitchItem: SwitchItemComponent = (props) => {
   const { children, href, ...rest } = props;
-  const router = useRouter();
-
-  let asPath = router.asPath;
-  if (asPath.slice(-1) === '/') {
-    asPath = asPath.slice(0, -1);
-  }
-
-  let active: boolean;
-  if (dynamics.ipfsMode) {
-    active = asPath.split(/[?#]/)[1] === href;
-  } else {
-    active = asPath.split(/[?#]/)[0] === href;
-  }
+  const active = useCompareWithRouterPath(href);
 
   return (
     <LocalLink href={href} style={{ zIndex: 2 }} {...rest}>
