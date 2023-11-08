77522e3c1f716a71b1b44b46aae55f064b4ac5d5
Dmitrii Podlesnyi <rezed93@gmail.com>
2023-11-08 17:14:49 +0400

    style: l2 banners design and texts updated

--- shared/banners/l2-swap/l2-swap.tsx
+++ shared/banners/l2-swap/l2-swap.tsx
@@ -1,49 +1,22 @@
-import { FC } from 'react';
-import { ThemeProvider, themeDark } from '@lidofinance/lido-ui';
 import { trackEvent } from '@lidofinance/analytics-matomo';
 import { MATOMO_CLICK_EVENTS } from 'config';
 
-import {
-  Wrapper,
-  L2Icons,
-  TextWrap,
-  ButtonWrap,
-  ButtonLinkWrap,
-  ButtonStyle,
-  ContentWrap,
-  TextHeader,
-} from './styles';
+import { L2Banner } from '../l2-banner';
 
 const CURVE_LINK = 'https://kyberswap.com/swap/ethereum/eth-to-wsteth';
 
 const linkClickHandler = () => trackEvent(...MATOMO_CLICK_EVENTS.l2swap);
 
-export const L2Swap: FC = () => {
-  const linkProps = {
-    href: CURVE_LINK,
-    target: '_blank',
-    rel: 'noopener noreferrer',
-  };
-
+export const L2Swap = () => {
   return (
-    <Wrapper data-testid="l2SwapBanner">
-      <ThemeProvider theme={themeDark}>
-        <ContentWrap>
-          <TextWrap>
-            <TextHeader>Swap ETH to wstETH on L2</TextHeader>
-            Swap ETH to wstETH directly on L2 and use wstETH in DeFi enjoying
-            low gas fees opportunities
-          </TextWrap>
-          <ButtonWrap>
-            <ButtonLinkWrap {...linkProps} onClick={linkClickHandler}>
-              <ButtonStyle data-testid="l2Swap" size="sm" color="secondary">
-                Swap
-              </ButtonStyle>
-            </ButtonLinkWrap>
-          </ButtonWrap>
-        </ContentWrap>
-        <L2Icons />
-      </ThemeProvider>
-    </Wrapper>
+    <L2Banner
+      title="Swap ETH to wstETH on L2"
+      text="Swap ETH to wstETH directly on L2 and use wstETH in DeFi enjoying low gas fees opportunities"
+      buttonText="Swap"
+      buttonHref={CURVE_LINK}
+      testidWrap="l2SwapBanner"
+      testidButton="l2Swap"
+      onClickButton={linkClickHandler}
+    />
   );
 };
