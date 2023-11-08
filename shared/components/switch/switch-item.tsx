4ee2e59ed32704e2d6e2fae50efac3e0165946d2
Anton Shalimov <a.shalimof@yandex.ru>
2023-11-08 11:26:56 +0300

    refactor: check active in switch item component

--- shared/components/switch/switch-item.tsx
+++ shared/components/switch/switch-item.tsx
@@ -1,26 +1,24 @@
+import { useMemo } from 'react';
 import { useRouter } from 'next/router';
 
 import { dynamics } from 'config';
 
 import { SwitchItemStyled } from './styles';
 import { SwitchItemComponent } from './types';
+import { checkActiveInIPFS, checkActiveInInfra } from './utils';
 import { LocalLink } from '../local-link';
 
 export const SwitchItem: SwitchItemComponent = (props) => {
   const { children, href, ...rest } = props;
   const router = useRouter();
 
-  let asPath = router.asPath;
-  if (asPath.slice(-1) === '/') {
-    asPath = asPath.slice(0, -1);
-  }
-
-  let active: boolean;
-  if (dynamics.ipfsMode) {
-    active = asPath.split(/[?#]/)[1] === href;
-  } else {
-    active = asPath.split(/[?#]/)[0] === href;
-  }
+  const active = useMemo(
+    () =>
+      dynamics.ipfsMode
+        ? checkActiveInIPFS(router.asPath, href)
+        : checkActiveInInfra(router.asPath, href),
+    [router.asPath, href],
+  );
 
   return (
     <LocalLink href={href} style={{ zIndex: 2 }} {...rest}>
