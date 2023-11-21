f7d1c57ae2bdc89f5b80726b59ea0ade8adbf343
Anton Shalimov <a.shalimof@yandex.ru>
2023-11-21 10:53:39 +0300

    refactor: code style

--- utilsApi/get-faq.ts
+++ utilsApi/get-faq.ts
@@ -3,7 +3,7 @@ import { standardFetcher } from 'utils/standardFetcher';
 import { responseTimeExternalMetricWrapper } from './fetchApiWrapper';
 
 export const getFaq = async (path: string): Promise<string | null> => {
-  console.debug('[getFaq] Started fetching from CMS...');
+  console.debug(`[getFaq] Started fetching the '${path}' from CMS...`);
   const cmsHost = serverRuntimeConfig.faqContentHost;
 
   const data = await responseTimeExternalMetricWrapper({
@@ -18,10 +18,10 @@ export const getFaq = async (path: string): Promise<string | null> => {
   });
 
   if (!data) {
-    console.error('[getFaq] Request to CMS failed');
+    console.error(`[getFaq] Request the '${path}' to CMS failed!`);
     return null;
   }
 
-  console.debug('[getFaq] Fetched from CMS was successes');
+  console.debug(`[getFaq] Fetched the '${path}' from CMS was successes!`);
   return data;
 };
