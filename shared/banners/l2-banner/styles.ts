a24978e62e482f39f5238c89c7c644925f7c7ff1
Dmitrii Podlesnyi <rezed93@gmail.com>
2023-12-06 17:30:04 +0400

    Revert "feat: l2 banners updated with zkSync"
    This reverts commit db942463e9bf770c21eafce9965fffd196cd9fd3.

--- shared/banners/l2-banner/styles.ts
+++ shared/banners/l2-banner/styles.ts
@@ -6,19 +6,14 @@ export const Wrapper = styled.div`
   margin-top: 16px;
   position: relative;
   display: flex;
-  flex-direction: column;
   text-align: left;
+  align-items: center;
   padding: ${({ theme }) => theme.spaceMap.lg}px;
   border-radius: ${({ theme }) => theme.borderRadiusesMap.lg}px;
-  gap: 10px;
+  gap: 20px;
   overflow: hidden;
   background-color: #07080c;
-  background: radial-gradient(
-    93.45% 103.1% at 6.55% 17.29%,
-    #3c64b6 0%,
-    #2e1d7b 55.75%,
-    #142698 100%
-  );
+  background: radial-gradient(120% 180% at 100% 100%, #62c4fb 0%, #112dbf 100%);
   box-sizing: border-box;
 
   ${({ theme }) => theme.mediaQueries.md} {
@@ -36,20 +31,22 @@ export const L2Icons = styled.img.attrs({
   src: Icons,
   alt: '',
 })`
+  position: relative;
   display: block;
-  width: 110px;
-  height: 32px;
+  width: 44px;
+  height: 120px;
 `;
 
-export const FooterWrap = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+export const ContentWrap = styled.div`
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
 `;
 
 export const TextHeader = styled.div`
+  margin-bottom: 4px;
   font-size: 14px;
-  line-height: 16px;
+  line-height: 24px;
   font-weight: 700;
   color: #fff;
 `;
@@ -61,6 +58,11 @@ export const TextWrap = styled.div`
   font-size: 12px;
   font-weight: 400;
   position: relative;
+`;
+
+export const ButtonWrap = styled.div`
+  display: flex;
+  margin-top: 12px;
 `;
 
 export const ButtonLinkWrap = styled.a`
@@ -76,11 +78,12 @@ export const ButtonLinkWrap = styled.a`
 `;
 
 export const ButtonStyle = styled(Button)`
+  background-color: #fff;
   padding: 7px 16px;
   font-size: 12px;
   line-height: 20px;
 
-  ${({ theme }) => theme.mediaQueries.sm} {
+  @media (max-width: 396px) {
     display: none;
   }
 `;
