d89cc4f7937ff812fcf2550e6435fda5bc84ec74
Anton Shalimov <a.shalimof@yandex.ru>
2023-12-07 13:05:26 +0300

    fix(IPFS links): opening in new tab

--- shared/components/link-ipfs.tsx
+++ shared/components/link-ipfs.tsx
@@ -1,4 +1,9 @@
-import React, { ReactNode, MouseEventHandler, useCallback } from 'react';
+import React, {
+  ReactNode,
+  MouseEventHandler,
+  useCallback,
+  useMemo,
+} from 'react';
 
 import { usePrefixedPush } from 'shared/hooks/use-prefixed-history';
 
@@ -13,6 +18,7 @@ export const LinkIpfs = ({ onClick, query, ...props }: Props) => {
   const push = usePrefixedPush();
   const { href } = props;
 
+  // Actual for click (opening in same tab)
   const handleClick: MouseEventHandler<HTMLAnchorElement> = useCallback(
     (event) => {
       event.preventDefault();
@@ -22,7 +28,19 @@ export const LinkIpfs = ({ onClick, query, ...props }: Props) => {
     [onClick, push, href, query],
   );
 
+  // Actual for opening in new tab
+  const basedHashHref = useMemo(() => {
+    let queryString = new URLSearchParams(query).toString();
+    queryString = queryString.length > 0 ? `?${queryString}` : '';
+
+    // Make a link:
+    // '/?<some_query_param>=<some_query_value>&...&<some_query_param_N>=<some_query_value_N>#/<hash_path>'
+    // e.g.: '/#/wrap'
+    // e.g.: '?ref=0x0000000000000000000000000000000000000000#/'
+    return `/${queryString}#${href}`;
+  }, [href, query]);
+
   // TODO:
   // eslint-disable-next-line jsx-a11y/anchor-has-content,jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
-  return <a {...props} onClick={handleClick} />;
+  return <a {...props} href={basedHashHref} onClick={handleClick} />;
 };
