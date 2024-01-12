594a0681dc4864c7677babb96156757144ffb9c6
Dmitrii Podlesnyi <rezed93@gmail.com>
2024-01-12 17:50:38 +0400

    feat: faq navigation scrolls smoothly, opens a text section and do not affect route in ipfs mode

--- features/withdrawals/request/form/bunker-info.tsx
+++ features/withdrawals/request/form/bunker-info.tsx
@@ -1,14 +1,18 @@
-import Link from 'next/link';
-
+import { useInpageNavigation } from 'providers/inpage-navigation';
 import { InfoBoxStyled } from 'features/withdrawals/shared';
 
 export const BunkerInfo = () => {
+  const { navigateInpageAnchor } = useInpageNavigation();
+
   return (
     <InfoBoxStyled>
       Lido protocol is in &quot;Bunker mode&quot;. The withdrawal requests are
       slowed down until the consequences of the incident that caused
       &quot;Bunker mode&quot; are not resolved. For more details,{' '}
-      <Link href="#bunkerModeScenarios">see here</Link>.
+      <a href="#bunkerModeScenarios" onClick={navigateInpageAnchor}>
+        see here
+      </a>
+      .
     </InfoBoxStyled>
   );
 };
