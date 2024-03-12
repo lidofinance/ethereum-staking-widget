d884376e2a60f488f65c15b7cfddb9162ed84745
Evgeny Taktarov <je.mouth@gmail.com>
2024-03-12 14:50:53 +0700

    fix: add autoconnect handling

--- shared/wallet/connect/connect.tsx
+++ shared/wallet/connect/connect.tsx
@@ -4,15 +4,30 @@ import { wrapWithEventTrack } from '@lidofinance/analytics-matomo';
 import { MATOMO_CLICK_EVENTS } from 'config';
 import { useClientConfig } from 'providers/client-config';
 import { useConnectWalletModal } from '../connect-wallet-modal/use-connect-wallet-modal';
+import { useConnectorInfo, useEagerConnect } from 'reef-knot/core-react';
 
 export const Connect: FC<ButtonProps> = (props) => {
   const { isWalletConnectionAllowed } = useClientConfig();
   const { onClick, ...rest } = props;
   const { openModal } = useConnectWalletModal();
+  const { isAutoConnectionSuitable } = useConnectorInfo();
+  const { eagerConnect } = useEagerConnect();
 
   const handleClick = wrapWithEventTrack(
     MATOMO_CLICK_EVENTS.connectWallet,
-    useCallback(() => openModal({}), [openModal]),
+    useCallback(() => {
+      if (!isWalletConnectionAllowed) return;
+      if (isAutoConnectionSuitable) {
+        void eagerConnect();
+      } else {
+        openModal({});
+      }
+    }, [
+      eagerConnect,
+      isAutoConnectionSuitable,
+      isWalletConnectionAllowed,
+      openModal,
+    ]),
   );
 
   return (
