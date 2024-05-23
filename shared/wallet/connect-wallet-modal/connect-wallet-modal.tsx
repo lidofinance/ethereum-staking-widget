7d8f7e37b3f08f2c77df96f19d08e60818c9bde2
Anton Shalimov <a.shalimof@yandex.ru>
2024-05-23 11:48:53 +0300

    feat: pass 'termsLink' and 'privacyNoticeLink' to reef-knot

--- shared/wallet/connect-wallet-modal/connect-wallet-modal.tsx
+++ shared/wallet/connect-wallet-modal/connect-wallet-modal.tsx
@@ -1,6 +1,8 @@
 import { useThemeToggle } from '@lidofinance/lido-ui';
 import { WalletsModalForEth } from 'reef-knot/connect-wallet-modal';
 import { WalletIdsEthereum } from 'reef-knot/wallets';
+
+import { config } from 'config';
 import { walletsMetrics } from 'consts/matomo-wallets-events';
 
 const WALLETS_PINNED: WalletIdsEthereum[] = ['okx', 'browserExtension'];
@@ -13,6 +15,8 @@ export const ConnectWalletModal = () => {
       shouldInvertWalletIcon={themeName === 'dark'}
       metrics={walletsMetrics}
       walletsPinned={WALLETS_PINNED}
+      termsLink={`${config.rootOrigin}/terms-of-use`}
+      privacyNoticeLink={`${config.rootOrigin}/privacy-notice`}
     />
   );
 };
