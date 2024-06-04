00e550d3da68e0f5e11c1bf46e732a48eb4959d9
Anton Shalimov <a.shalimof@yandex.ru>
2024-06-04 15:06:56 +0300

    fix: index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,6 @@ import { GetStaticProps } from 'next';
 import { config } from 'config';
 import { StakePage } from 'features/stake';
 import { HomePageIpfs } from 'features/ipfs';
-import { GetStaticProps } from 'next';
-
-export const getStaticProps: GetStaticProps = async () => {
-  return { props: {} };
-};
 
 export default config.ipfsMode ? HomePageIpfs : StakePage;
 
