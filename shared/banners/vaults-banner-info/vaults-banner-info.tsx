323533b6a31dae634cb531c234ab7f5ad0744c9b
Dmitrii Podlesnyi <rezed93@gmail.com>
2024-06-05 16:10:23 +0700

    chore: vaults banner text updated

--- shared/banners/vaults-banner-info/vaults-banner-info.tsx
+++ shared/banners/vaults-banner-info/vaults-banner-info.tsx
@@ -3,12 +3,12 @@ import { useThemeToggle } from '@lidofinance/lido-ui';
 import { BannerLinkButton } from '../banner-link-button';
 import { Wrap, Title, Description, Footer, Logos } from './styles';
 
-import { ReactComponent as IconGauntletDark } from 'assets/vault-banner/icon-gauntlet-dark.svg';
+import { ReactComponent as IconMevDark } from 'assets/vault-banner/icon-mev-dark.svg';
 import { ReactComponent as IconP2PDark } from 'assets/vault-banner/icon-p2p-dark.svg';
 import { ReactComponent as IconRe7Dark } from 'assets/vault-banner/icon-re7-dark.svg';
 import { ReactComponent as IconStakehouseDark } from 'assets/vault-banner/icon-stakehouse-dark.svg';
 
-import { ReactComponent as IconGauntletLight } from 'assets/vault-banner/icon-gauntlet-light.svg';
+import { ReactComponent as IconMevLight } from 'assets/vault-banner/icon-mev-light.svg';
 import { ReactComponent as IconP2PLight } from 'assets/vault-banner/icon-p2p-light.svg';
 import { ReactComponent as IconRe7Light } from 'assets/vault-banner/icon-re7-light.svg';
 import { ReactComponent as IconStakehouseLight } from 'assets/vault-banner/icon-stakehouse-light.svg';
@@ -25,10 +25,10 @@ export const VaultsBannerInfo = () => {
   const { themeName } = useThemeToggle();
   return (
     <Wrap>
-      <Title>Get more with restaking vaults</Title>
+      <Title>Explore restaking opportunities</Title>
       <Description>
-        Use stETH to unlock restaking rewards through the best-in-class
-        opinionated curated vaults
+        Use stETH to unlock restaking rewards through a set of carefully curated
+        vaults
       </Description>
       <Footer>
         <Logos>
@@ -39,7 +39,7 @@ export const VaultsBannerInfo = () => {
             <IconStakehouseLight />
           )}
           {themeName === 'dark' ? <IconRe7Dark /> : <IconRe7Light />}
-          {themeName === 'dark' ? <IconGauntletDark /> : <IconGauntletLight />}
+          {themeName === 'dark' ? <IconMevDark /> : <IconMevLight />}
         </Logos>
         <BannerLinkButton href={LINK_LEARN_MORE} onClick={linkClickHandler}>
           Learn more
