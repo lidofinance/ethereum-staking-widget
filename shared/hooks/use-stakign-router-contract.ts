dcc0a44b4ebaf00d2bdec19fee72c2925a6e0465
DiRaiks <t.fane@mail.ru>
2024-06-10 14:45:59 +0300

    fix: remove web3 hook

--- shared/hooks/use-stakign-router-contract.ts
+++ shared/hooks/use-stakign-router-contract.ts
@@ -1,8 +1,7 @@
 import { stakingRouter } from 'consts/staking-router';
 
 export const useStakingRouter = () => {
-  const contractWeb3 = stakingRouter.useContractWeb3();
   const contractRpc = stakingRouter.useContractRPC();
 
-  return { contractWeb3, contractRpc };
+  return { contractRpc };
 };
