033503ee07a61612780209757edc8d10b4393d89
Anton Shalimov <a.shalimof@yandex.ru>
2024-06-24 22:58:52 +0300

    feat: unsupported chain button

--- features/stake/stake-form/controls/stake-submit-button.tsx
+++ features/stake/stake-form/controls/stake-submit-button.tsx
@@ -1,12 +1,17 @@
+import { useWeb3 } from 'reef-knot/web3-react';
 import { SubmitButtonHookForm } from 'shared/hook-form/controls/submit-button-hook-form';
-import { useStakeFormData } from '../stake-form-context';
 import { LIMIT_LEVEL } from 'types';
+import { useStakeFormData } from '../stake-form-context';
 
 export const StakeSubmitButton = () => {
+  const { active } = useWeb3();
   const { stakingLimitInfo } = useStakeFormData();
+
   return (
     <SubmitButtonHookForm
-      disabled={stakingLimitInfo?.stakeLimitLevel === LIMIT_LEVEL.REACHED}
+      disabled={
+        !active || stakingLimitInfo?.stakeLimitLevel === LIMIT_LEVEL.REACHED
+      }
       data-testid="stakeSubmitBtn"
       errorField="amount"
     >
