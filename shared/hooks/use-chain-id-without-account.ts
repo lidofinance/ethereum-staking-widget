5dc988b724ae9ac15b97bf612753ba6f79a56c9c
Anton Shalimov <a.shalimof@yandex.ru>
2024-07-01 19:52:41 +0300

    feat: add 'useChainIdWithoutAccount' hook

---
+++ shared/hooks/use-chain-id-without-account.ts
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+
+const checkWindowDotEthereum = () =>
+  Object.prototype.hasOwnProperty.call(window, 'ethereum');
+
+export const useChainIdWithoutAccount = () => {
+  const [chainId, setChainId] = useState<number | null>(null);
+
+  useEffect(() => {
+    const fetchChainId = async () => {
+      if (!checkWindowDotEthereum()) return;
+
+      try {
+        const chainIdHex = await window.ethereum.request({
+          method: 'eth_chainId',
+        });
+        const chainId = parseInt(chainIdHex, 16);
+        setChainId(chainId);
+      } catch {
+        /* noop */
+      }
+    };
+
+    void fetchChainId();
+
+    // Handler for chain changes
+    const handleChainChanged = (chainIdHex) => {
+      const chainId = parseInt(chainIdHex, 16);
+      setChainId(chainId);
+    };
+
+    if (checkWindowDotEthereum()) {
+      window.ethereum.on('chainChanged', handleChainChanged);
+    }
+
+    // Cleanup listener on component unmount
+    return () => {
+      if (!checkWindowDotEthereum()) return;
+
+      window.ethereum.removeListener('chainChanged', handleChainChanged);
+    };
+  }, []);
+
+  return chainId;
+};
