4b13b87cc38ca83d4893adaedcd159f9a3671ff7
Dmitrii Podlesnyi <rezed93@gmail.com>
2024-07-01 16:56:18 +0700

    feat: wagmi version updated

--- providers/sdk-legacy.tsx
+++ providers/sdk-legacy.tsx
@@ -1,86 +1,65 @@
-import React, { PropsWithChildren, useEffect, useMemo, useState } from 'react';
-import { ProviderSDK } from '@lido-sdk/react';
-import { getStaticRpcBatchProvider } from '@lido-sdk/providers';
+import React, { PropsWithChildren, useMemo } from 'react';
+import { useSupportedChains, useWeb3 } from 'reef-knot/web3-react';
+import { useConnectorClient } from 'wagmi';
+
 import { Web3Provider } from '@ethersproject/providers';
-import { Chain, useAccount } from 'wagmi';
+import { ProviderSDK } from '@lido-sdk/react';
+
 import { mainnet } from 'wagmi/chains';
-import { useWeb3 } from 'reef-knot/web3-react';
-import { onRpcProviderError } from 'utils/rpc-error-handler';
+import { getStaticRpcBatchProvider } from '@lido-sdk/providers';
+import { useReefKnotContext } from 'reef-knot/core-react';
 
 const POLLING_INTERVAL = 12_000;
 
 type SDKLegacyProviderProps = PropsWithChildren<{
   defaultChainId: number;
-  supportedChains: Chain[];
-  rpc: Record<number, string>;
   pollingInterval?: number;
 }>;
 
 export const SDKLegacyProvider = ({
   children,
   defaultChainId,
-  rpc,
-  supportedChains,
   pollingInterval = POLLING_INTERVAL,
 }: SDKLegacyProviderProps) => {
-  const { chainId = defaultChainId, account } = useWeb3();
-  const { connector, isConnected } = useAccount();
+  const { chainId = defaultChainId, account, active } = useWeb3();
+  const { supportedChains } = useSupportedChains();
+  const { data: client } = useConnectorClient();
+  const { rpc } = useReefKnotContext();
 
-  const [providerWeb3, setProviderWeb3] = useState<Web3Provider>();
+  const providerWeb3 = useMemo(() => {
+    if (!client || !client.account || !active) return;
+    const { chain, transport } = client;
 
-  // Reset web3 provider if the provider was set previously,
-  // and currently no wallet is connected.
-  // Gets triggered on a wallet disconnection, for example.
-  if (!isConnected && providerWeb3) {
-    setProviderWeb3(undefined);
-  }
+    // https://wagmi.sh/core/guides/ethers#reference-implementation-1
+    const network = {
+      chainId: chain.id,
+      name: chain.name,
+      ensAddress: chain.contracts?.ensRegistry?.address,
+    };
+    const provider = new Web3Provider(transport, network);
+    provider.pollingInterval = pollingInterval;
 
-  useEffect(() => {
-    void (async () => {
-      if (!providerWeb3 && connector && isConnected) {
-        const provider = await connector.getProvider();
-        // `any` param + page reload on network change
-        // are described here: https://github.com/ethers-io/ethers.js/issues/866
-        // this approach is needed to fix a NETWORK_ERROR after chain changing
-        const wrappedProvider = new Web3Provider(provider, 'any');
-        wrappedProvider.on('network', (newNetwork, oldNetwork) => {
-          // When a Provider makes its initial connection, it emits a "network"
-          // event with a null oldNetwork along with the newNetwork. So, if the
-          // oldNetwork exists, it represents a changing network
-          if (oldNetwork) {
-            window.location.reload();
-          }
-        });
-        wrappedProvider.pollingInterval = pollingInterval;
-        setProviderWeb3(wrappedProvider);
-      }
-    })();
-  }, [connector, isConnected, pollingInterval, providerWeb3]);
+    return provider;
+  }, [active, client, pollingInterval]);
 
   const supportedChainIds = useMemo(
-    () => supportedChains.map((chain) => chain.id),
+    () => supportedChains.map((chain) => chain.chainId),
     [supportedChains],
   );
 
-  const { providerRpc, providerMainnetRpc } = useMemo(() => {
-    const providerRpc = getStaticRpcBatchProvider(
-      chainId,
-      rpc[chainId],
-      chainId,
-      pollingInterval,
-    );
+  const providerRpc = getStaticRpcBatchProvider(
+    chainId,
+    rpc[chainId],
+    0,
+    POLLING_INTERVAL,
+  );
 
-    const providerMainnetRpc = getStaticRpcBatchProvider(
-      mainnet.id,
-      rpc[mainnet.id],
-      mainnet.id,
-      pollingInterval,
-    );
-    providerRpc.on('debug', onRpcProviderError);
-    providerMainnetRpc.on('debug', onRpcProviderError);
-
-    return { providerRpc, providerMainnetRpc };
-  }, [chainId, pollingInterval, rpc]);
+  const providerMainnetRpc = getStaticRpcBatchProvider(
+    mainnet.id,
+    rpc[mainnet.id],
+    0,
+    POLLING_INTERVAL,
+  );
 
   return (
     // @ts-expect-error Property children does not exist on type
