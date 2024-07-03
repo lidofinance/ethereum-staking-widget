ce37db2f144968b0293b82b53c42b3758c83b0a1
Anton Shalimov <a.shalimof@yandex.ru>
2024-07-03 10:08:35 +0300

    feat: switchChain

--- shared/wallet/fallback/fallback.tsx
+++ shared/wallet/fallback/fallback.tsx
@@ -1,12 +1,17 @@
+import { useSwitchChain } from 'wagmi';
+import { mainnet } from '@wagmi/core/chains';
 import { useSDK } from '@lido-sdk/react';
+
 import { CHAINS } from 'consts/chains';
 import { WalletCardComponent } from 'shared/wallet/card/types';
+
 import { FallbackWalletStyle, TextStyle, ButtonStyle } from './styles';
 import { useErrorMessage } from './useErrorMessage';
 
 export const Fallback: WalletCardComponent = (props) => {
   const error = useErrorMessage();
   const { supportedChainIds } = useSDK();
+  const { switchChain } = useSwitchChain();
 
   // Show mainnet if there is in supportedChainIds
   let switchToChain;
@@ -23,7 +28,12 @@ export const Fallback: WalletCardComponent = (props) => {
     return (
       <FallbackWalletStyle {...props}>
         <TextStyle>{error}</TextStyle>
-        <ButtonStyle size={'xs'}>Switch to {switchToChain}</ButtonStyle>
+        <ButtonStyle
+          size={'xs'}
+          onClick={() => switchChain({ chainId: mainnet.id })}
+        >
+          Switch to {switchToChain}
+        </ButtonStyle>
       </FallbackWalletStyle>
     );
   }
