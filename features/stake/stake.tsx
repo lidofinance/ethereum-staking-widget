a6f224df93146a5885063b808d47724db009ae9f
Anton Shalimov <a.shalimof@yandex.ru>
2024-07-03 10:13:36 +0300

    chore: remove demo and todos

--- features/stake/stake.tsx
+++ features/stake/stake.tsx
@@ -1,5 +1,3 @@
-import { useErrorBoundary } from 'react-error-boundary';
-
 import { FaqPlaceholder } from 'features/ipfs';
 import { useWeb3Key } from 'shared/hooks/useWeb3Key';
 import NoSSRWrapper from 'shared/components/no-ssr-wrapper';
@@ -12,18 +10,9 @@ import { StakeForm } from './stake-form';
 
 export const Stake = () => {
   const key = useWeb3Key();
-  const { showBoundary } = useErrorBoundary();
 
   return (
     <>
-      {/* TODO: DEMO, delete later */}
-      <button
-        onClick={() => {
-          showBoundary('Stake -> <> -> button');
-        }}
-      >
-        Throw new Error (1)
-      </button>
       <GoerliSunsetBanner />
       <NoSSRWrapper>
         <StakeForm key={key} />
