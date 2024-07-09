20d1905307b50fd765bfb8677dbb6dc44eb679a4
Anton Shalimov <a.shalimof@yandex.ru>
2024-07-10 01:00:31 +0300

    refactor(wallet fallback): not use web3-react

--- shared/wallet/fallback/useErrorMessage.ts
+++ shared/wallet/fallback/useErrorMessage.ts
@@ -2,11 +2,11 @@ import {
   useConnectorInfo,
   getUnsupportedChainError,
 } from 'reef-knot/core-react';
-import { helpers, useSupportedChains, useWeb3 } from 'reef-knot/web3-react';
-import { useAccount, useConfig } from 'wagmi';
+import { helpers, useSupportedChains } from 'reef-knot/web3-react';
+import { useAccount, useConfig, useConnect } from 'wagmi';
 
 export const useErrorMessage = (): string | undefined => {
-  const { error } = useWeb3();
+  const { error } = useConnect();
   const { chains } = useConfig();
   const { isConnected } = useAccount();
   const { isUnsupported } = useSupportedChains();
