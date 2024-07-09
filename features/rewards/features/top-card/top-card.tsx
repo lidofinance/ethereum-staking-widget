3da37267a86f18a0acaeac90d3502a02789b2e50
Anton Shalimov <a.shalimof@yandex.ru>
2024-07-10 00:27:10 +0300

    refactor(reward top-card): simplify to read of source code and not use web3-react

--- features/rewards/features/top-card/top-card.tsx
+++ features/rewards/features/top-card/top-card.tsx
@@ -1,21 +1,25 @@
 import { FC } from 'react';
-import { useWeb3 } from 'reef-knot/web3-react';
 import { useAccount } from 'wagmi';
 
 import { StatsWrapper } from 'features/rewards/components/statsWrapper';
 import { Stats } from 'features/rewards/components/stats';
 import { Fallback } from 'shared/wallet';
+import { useIsConnectedWalletAndSupportedChain } from 'shared/hooks/use-is-connected-wallet-and-supported-chain';
 
 import { Wallet } from './wallet';
 import { ConnectWallet } from './connect-wallet';
 
 export const TopCard: FC = () => {
-  const { address } = useAccount();
-  const { active } = useWeb3();
+  const { isConnected } = useAccount();
+  const isActiveWallet = useIsConnectedWalletAndSupportedChain();
 
   return (
     <>
-      {address ? active ? <Wallet /> : <Fallback /> : <ConnectWallet />}
+      {!isConnected && <ConnectWallet />}
+
+      {isConnected && !isActiveWallet && <Fallback />}
+
+      {isConnected && isActiveWallet && <Wallet />}
       <StatsWrapper>
         <Stats />
       </StatsWrapper>
