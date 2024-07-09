3c19e24869b8aadd686a31185804bd876ea0b3c3
Anton Shalimov <a.shalimof@yandex.ru>
2024-07-10 00:31:33 +0300

    refactor(stake-submit-button): not use web3-react

--- features/stake/stake-form/controls/stake-submit-button.tsx
+++ features/stake/stake-form/controls/stake-submit-button.tsx
@@ -1,16 +1,18 @@
-import { useWeb3 } from 'reef-knot/web3-react';
-import { SubmitButtonHookForm } from 'shared/hook-form/controls/submit-button-hook-form';
 import { LIMIT_LEVEL } from 'types';
+import { SubmitButtonHookForm } from 'shared/hook-form/controls/submit-button-hook-form';
+import { useIsConnectedWalletAndSupportedChain } from 'shared/hooks/use-is-connected-wallet-and-supported-chain';
+
 import { useStakeFormData } from '../stake-form-context';
 
 export const StakeSubmitButton = () => {
-  const { active } = useWeb3();
+  const isActiveWallet = useIsConnectedWalletAndSupportedChain();
   const { stakingLimitInfo } = useStakeFormData();
 
   return (
     <SubmitButtonHookForm
       disabled={
-        !active || stakingLimitInfo?.stakeLimitLevel === LIMIT_LEVEL.REACHED
+        !isActiveWallet ||
+        stakingLimitInfo?.stakeLimitLevel === LIMIT_LEVEL.REACHED
       }
       data-testid="stakeSubmitBtn"
       errorField="amount"
