51968f21a730ffa8b069698706a7559bc154c606
Evgeny Taktarov <je.mouth@gmail.com>
2024-07-11 14:28:40 +0700

    fix: case for no rewards yet

--- features/rewards/components/errorBlocks/ErrorBlockNoSteth.tsx
+++ features/rewards/components/errorBlocks/ErrorBlockNoSteth.tsx
@@ -1,23 +1,36 @@
+import { Zero } from '@ethersproject/constants';
+import { useSTETHBalance } from '@lido-sdk/react';
 import { Box, Button } from '@lidofinance/lido-ui';
+import { STRATEGY_LAZY } from 'consts/swr-strategies';
 import { HOME_PATH } from 'consts/urls';
 import { LocalLink } from 'shared/components/local-link';
 
-export const ErrorBlockNoSteth = () => (
-  <Box
-    id="nothingStaked"
-    display="flex"
-    flexDirection="column"
-    alignItems="center"
-    pt="32px"
-    pb="18px"
-  >
-    <Box textAlign="center" pb="12px">
-      You don&apos;t have staked tokens. Stake now and receive daily rewards.
-    </Box>
-    <LocalLink href={HOME_PATH}>
-      <Box width="150px">
-        <Button>Stake now</Button>
+export const ErrorBlockNoSteth = () => {
+  const { data } = useSTETHBalance(STRATEGY_LAZY);
+  const hasSteth = data?.gt(Zero);
+  const text = hasSteth
+    ? "You haven't received rewards on your staked tokens yet. Please check back later to see your rewards."
+    : "You don't have staked tokens. Stake now and receive daily rewards.";
+
+  return (
+    <Box
+      id="nothingStaked"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      pt="32px"
+      pb="18px"
+    >
+      <Box textAlign="center" pb="12px">
+        {text}
       </Box>
-    </LocalLink>
-  </Box>
-);
+      <LocalLink href={HOME_PATH}>
+        {!hasSteth && (
+          <Box width="150px">
+            <Button>Stake now</Button>
+          </Box>
+        )}
+      </LocalLink>
+    </Box>
+  );
+};
