789ea0d31f5adc78f57d9bbd39d1f37e05e17a31
Anton Shalimov <a.shalimof@yandex.ru>
2024-07-15 09:56:18 +0300

    refactor: get error connect from wagmi

--- shared/wallet/fallback/useErrorMessage.ts
+++ shared/wallet/fallback/useErrorMessage.ts
@@ -2,13 +2,14 @@ import {
   useConnectorInfo,
   getUnsupportedChainError,
 } from 'reef-knot/core-react';
-import { helpers, useSupportedChains, useWeb3 } from 'reef-knot/web3-react';
-import { useAccount, useConfig } from 'wagmi';
+import { helpers, useSupportedChains } from 'reef-knot/web3-react';
+import { useAccount, useConnect, useConfig } from 'wagmi';
 
 export const useErrorMessage = (): string | undefined => {
-  const { error } = useWeb3();
   const { chains } = useConfig();
   const { isConnected } = useAccount();
+  const { error } = useConnect();
+
   const { isUnsupported } = useSupportedChains();
   const { isLedger } = useConnectorInfo();
 
