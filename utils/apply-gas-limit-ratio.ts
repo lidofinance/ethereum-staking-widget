37735e7615ca0c21eaf14f0bc9b6c938f256c722
Andrew Finaev <t.fane@mail.ru>
2024-07-25 17:49:24 +0300

    Revert "[fix] remove usage of wallet rpc during tx"

--- utils/apply-gas-limit-ratio.ts
+++
@@ -1,7 +0,0 @@
-import { config } from 'config';
-import type { BigNumber } from 'ethers';
-
-export const applyGasLimitRatio = (gasLimit: BigNumber): BigNumber =>
-  gasLimit
-    .mul(config.SUBMIT_EXTRA_GAS_TRANSACTION_RATIO * config.PRECISION)
-    .div(config.PRECISION);
