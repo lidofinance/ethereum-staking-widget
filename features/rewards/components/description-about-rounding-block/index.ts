d034f3f7bff41e79ce00507ebad3acdae3125be8
Anton Shalimov <a.shalimof@yandex.ru>
2024-08-02 16:34:43 +0300

    refactor: wallet block

--- features/rewards/components/description-about-rounding-block/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './description-about-rounding-block';
--- features/rewards/features/top-card/wallet.tsx
+++ features/rewards/features/top-card/wallet.tsx
@@ -1,10 +1,8 @@
 import { FC } from 'react';
 import { Box, ThemeProvider, themeDark } from '@lidofinance/lido-ui';
 
-import EthSymbol from 'features/rewards/components/EthSymbol';
 import NumberFormat from 'features/rewards/components/NumberFormat';
 import { Title } from 'features/rewards/components/stats/Title';
-import { DescriptionAboutRoundingBlock } from 'features/rewards/components/description-about-rounding-block';
 import { useRewardsHistory } from 'features/rewards/hooks';
 import { useRewardsBalanceData } from 'features/rewards/hooks/use-rewards-balance-data';
 import { FlexCenter } from 'features/stake/stake-form/wallet/styles';
@@ -36,18 +34,18 @@ export const Wallet: FC = () => {
               loading={loading}
               value={
                 <div data-testid="stEthBalance">
-                  <EthSymbol />
                   <NumberFormat
                     number={balanceData?.stEthBalanceParsed}
                     pending={loading}
                   />
+                  <Box display="inline-block" pl={'3px'}>
+                    stETH
+                  </Box>
                 </div>
               }
             >
               <Title data-testid="stEthBalanceIn$" hideMobile>
-                <Box display="inline-block" pr="3px">
-                  {currency.symbol}
-                </Box>
+                <Box display="inline-block">{currency.symbol}</Box>
                 <NumberFormat
                   number={balanceData?.stEthCurrencyBalance}
                   currency
@@ -59,14 +57,9 @@ export const Wallet: FC = () => {
             <WalletContentAddressBadgeStyle
               address={address as `0x${string}`}
               symbolsMobile={6}
-              symbolsDesktop={12}
+              symbolsDesktop={6}
             />
           </WalletContentRowStyle>
-
-          <DescriptionAboutRoundingBlock>
-            Current balance may differ from last balance in the table due to
-            rounding.
-          </DescriptionAboutRoundingBlock>
         </WalletContentStyle>
       </ThemeProvider>
     </WalletStyle>
