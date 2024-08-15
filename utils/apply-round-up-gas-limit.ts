b4cf798ff9c5fcaf10812e5837c2836f5ab56ef6
Anton Shalimov <a.shalimof@yandex.ru>
2024-08-15 11:58:29 +0300

    refactor: small optimization

--- utils/apply-round-up-gas-limit.ts
+++ utils/apply-round-up-gas-limit.ts
@@ -1,8 +1,9 @@
 import { BigNumber } from 'ethers';
 
-export const applyRoundUpGasLimit = (number: BigNumber): BigNumber => {
-  const bn1000 = BigNumber.from(1000);
+const bn1000 = BigNumber.from(1000);
+const bn999 = BigNumber.from(999);
 
+export const applyRoundUpGasLimit = (number: BigNumber): BigNumber => {
   // 94567 -> 94 -> 94000 -> 94999
-  return number.div(bn1000).mul(bn1000).add(BigNumber.from(999));
+  return number.div(bn1000).mul(bn1000).add(bn999);
 };
