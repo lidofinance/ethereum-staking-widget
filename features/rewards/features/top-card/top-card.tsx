564c9a1f7e94bc8488980e07d91af5a4286bf09d
Anton Shalimov <a.shalimof@yandex.ru>
2024-08-20 12:57:51 +0300

    refactor(reward): top-card

--- features/rewards/features/top-card/top-card.tsx
+++ features/rewards/features/top-card/top-card.tsx
@@ -2,23 +2,22 @@ import { FC, useEffect, useState } from 'react';
 
 import { StatsWrapper } from 'features/rewards/components/statsWrapper';
 import { Stats } from 'features/rewards/components/stats';
-import { useDappStatus } from 'shared/hooks/use-dapp-status';
 
 import { Wallet } from './wallet';
 
 export const TopCard: FC = () => {
   const [visible, setVisible] = useState(false);
-  const { isDappActive } = useDappStatus();
 
   // fix flash after reload page
   useEffect(() => {
     setVisible(true);
   }, []);
 
-  return !visible ? null : (
-    <>
-      {isDappActive && <Wallet />}
+  if (!visible) return null;
 
+  return (
+    <>
+      <Wallet />
       <StatsWrapper>
         <Stats />
       </StatsWrapper>
