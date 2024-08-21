4efc5ad72728f3adfcf98543e8bcaef2f0e9736c
Anton Shalimov <a.shalimof@yandex.ru>
2024-08-21 13:35:27 +0300

    feat(rewards page): unsupported banner

--- features/rewards/features/top-card/top-card.tsx
+++ features/rewards/features/top-card/top-card.tsx
@@ -2,11 +2,14 @@ import { FC, useEffect, useState } from 'react';
 
 import { StatsWrapper } from 'features/rewards/components/statsWrapper';
 import { Stats } from 'features/rewards/components/stats';
+import { useDappStatus } from 'shared/hooks/use-dapp-status';
+import { Fallback } from 'shared/wallet';
 
 import { Wallet } from './wallet';
 
 export const TopCard: FC = () => {
   const [visible, setVisible] = useState(false);
+  const { isWalletConnected, isSupportedChain } = useDappStatus();
 
   // fix flash after reload page
   useEffect(() => {
@@ -17,7 +20,8 @@ export const TopCard: FC = () => {
 
   return (
     <>
-      <Wallet />
+      {isWalletConnected && !isSupportedChain ? <Fallback /> : <Wallet />}
+
       <StatsWrapper>
         <Stats />
       </StatsWrapper>
