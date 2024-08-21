f6262285b0d7cbd93ce1191fb165363f5e59f07c
Anton Shalimov <a.shalimof@yandex.ru>
2024-08-21 23:12:20 +0300

    fix: condition to showing filters, currency select and export button

--- features/rewards/components/rewardsListHeader/RewardsListHeader.tsx
+++ features/rewards/components/rewardsListHeader/RewardsListHeader.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { useRewardsHistory } from 'features/rewards/hooks';
+import { useDappStatus } from 'shared/hooks/use-dapp-status';
 
 import { LeftOptions } from './LeftOptions';
 import { RightOptions } from './RightOptions';
@@ -7,17 +8,21 @@ import { RewardsListHeaderStyle } from './styles';
 import { TitleStyle } from './styles';
 
 export const RewardsListHeader: FC = () => {
+  const { isWalletConnected, isSupportedChain } = useDappStatus();
   const { error, data } = useRewardsHistory();
 
   return (
     <RewardsListHeaderStyle data-testid="rewardsHeader">
       <TitleStyle>Reward history</TitleStyle>
-      {!error && data && data?.events.length > 0 && (
-        <>
-          <LeftOptions />
-          <RightOptions />
-        </>
-      )}
+      {!error &&
+        data &&
+        data?.events.length > 0 &&
+        (!isWalletConnected || (isWalletConnected && isSupportedChain)) && (
+          <>
+            <LeftOptions />
+            <RightOptions />
+          </>
+        )}
     </RewardsListHeaderStyle>
   );
 };
