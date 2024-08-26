9245e790b3ea93581cec3a55b9338f211947918b
Evgeny Taktarov <je.mouth@gmail.com>
2024-08-26 20:14:46 +0700

    chore: naming

--- config/groups/revalidation.ts
+++ config/groups/revalidation.ts
@@ -1,7 +1,7 @@
 import type { ManifestConfig, ManifestEntry } from 'config/external-config';
 
 export const DEFAULT_REVALIDATION = 60 * 15; // 15 minutes
-export const ERROR_REVALIDATION = 60; // 1 minute
+export const ERROR_REVALIDATION_SECONDS = 60; // 1 minute
 
 export const FALLBACK_CONFIG: ManifestConfig = {
   enabledWithdrawalDexes: [],
