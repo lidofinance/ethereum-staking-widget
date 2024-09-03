18d09e6f82947924d1c98c8dbb8a4b4a02c79bd0
Anton Shalimov <a.shalimof@yandex.ru>
2024-09-03 09:10:01 +0300

    feat: add env vars logging, rpc startup checks, new metrics
    * feat: add ENV logs
    
    * feat: push open ENV vars to prom
    
    * refactor: simplify Metrics class
    
    * refactor: use the openKeys in the startup metrics
    
    * feat: add startup check RPC
    
    * fix: log level
    
    * feat: check all rpc
    
    * refactor: dirs
    
    * feat: remove RUN_STARTUP_CHECKS from 'yarn dev'
    
    * feat: pass startup-rpc-checks from mjs to Metrics class (ts source code)
    
    * feat: rpc startup checks metrics
    
    * feat: viem instead of ethers
    
    * chore: remove debug
    
    * refactor: rpc timeout 10 seconds
    
    * feat: remove stopping the app

---
+++ scripts/startup-checks/rpc.mjs
@@ -0,0 +1,78 @@
+import { createClient, http } from 'viem';
+import { getChainId } from 'viem/actions'
+
+// Safely initialize a global variable
+let globalRPCCheckResults = globalThis.__rpcCheckResults || [];
+globalThis.__rpcCheckResults = globalRPCCheckResults;
+
+export const BROKEN_URL = 'BROKEN_URL';
+export const RPC_TIMEOUT_SECONDS = 10_000;
+
+const pushRPCCheckResult = (domain, success) => {
+  globalRPCCheckResults.push({ domain, success });
+};
+
+export const getRPCCheckResults = () => globalThis.__rpcCheckResults || [];
+
+const getRpcUrls = (chainId) => {
+  const rpcUrls = process.env[`EL_RPC_URLS_${chainId}`]?.split(',');
+  return rpcUrls?.filter((url) => url);
+};
+
+export const startupCheckRPCs = async () => {
+  console.info('[startupCheckRPCs] Starting...');
+
+  try {
+    const defaultChain = parseInt(process.env.DEFAULT_CHAIN, 10);
+    const rpcUrls = getRpcUrls(defaultChain);
+
+    if (!rpcUrls || rpcUrls.length === 0) {
+      throw new Error('[startupCheckRPCs] No RPC URLs found!');
+    }
+
+    let errorCount = 0;
+
+    for (const url of rpcUrls) {
+      let domain;
+      try {
+        domain = new URL(url).hostname;
+      } catch (err) {
+        errorCount += 1;
+        console.error('There is a broken URL.');
+        pushRPCCheckResult(BROKEN_URL, false);
+        continue;
+      }
+
+      try {
+        const client = createClient({
+          transport: http(url, { retryCount: 0, timeout: RPC_TIMEOUT_SECONDS })
+        });
+
+        const chainId = await getChainId(client);
+
+        if (defaultChain === chainId) {
+          pushRPCCheckResult(domain, true);
+          console.info(`[startupCheckRPCs] RPC ${domain} works!`);
+        } else {
+          throw(`[startupCheckRPCs] RPC ${domain} does not work!`);
+        }
+      } catch (err) {
+        errorCount += 1;
+        pushRPCCheckResult(domain, false);
+        console.error(`[startupCheckRPCs] Error with RPC ${domain}:`);
+        console.error(String(err).replaceAll(rpcUrls, domain));
+        console.error(`[startupCheckRPCs] Timeout: ${RPC_TIMEOUT_SECONDS} seconds`);
+      }
+    }
+
+    if (errorCount > 0) {
+      console.info(`[startupCheckRPCs] Number of working RPCs: ${rpcUrls.length - errorCount}`);
+      console.info(`[startupCheckRPCs] Number of broken RPCs: ${errorCount}`);
+    } else {
+      console.info('[startupCheckRPCs] All RPC works!');
+    }
+  } catch (err) {
+    console.error('[startupCheckRPCs] Error during startup check:');
+    console.error(err);
+  }
+};
