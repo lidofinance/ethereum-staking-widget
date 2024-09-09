b1b29b081178dad2da9d998a341dbac01aec5820
DiRaiks <t.fane@mail.ru>
2024-09-09 14:44:37 +0300

    fix: add opener policy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { memo, useEffect } from 'react';
+import { memo } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { AppProps } from 'next/app';
 import 'nprogress/nprogress.css';
@@ -31,12 +31,6 @@ nprogress();
 
 const App = (props: AppProps) => {
   const { Component, pageProps } = props;
-
-  useEffect(() => {
-    if (window.opener) {
-      window.opener = null;
-    }
-  }, []);
 
   return (
     <ErrorBoundary fallbackRender={ErrorBoundaryFallback}>
