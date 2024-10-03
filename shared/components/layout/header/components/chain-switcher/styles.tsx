8c14a722f6d00f7dec484d6f229b90ea9074a328
Anton Shalimov <a.shalimof@yandex.ru>
2024-10-03 21:30:18 +0300

    feat: hover of SelectIconStyled

--- shared/components/layout/header/components/chain-switcher/styles.tsx
+++ shared/components/layout/header/components/chain-switcher/styles.tsx
@@ -1,9 +1,22 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { SelectIcon } from '@lidofinance/lido-ui';
 
 export const SelectIconStyled = styled(SelectIcon)`
   overflow: ${({ disabled }) => (disabled ? 'hidden' : 'visible')};
   width: ${({ disabled }) => (disabled ? '44px' : 'auto')};
+
+  &:not(:disabled):hover {
+    & > span {
+      ${({ theme, disabled }) =>
+        theme.name === 'dark'
+          ? css`
+              background: ${!disabled && '#34343D'};
+            `
+          : css`
+              background: ${!disabled && '#000A3D08'};
+            `}
+    }
+  }
 
   & > span {
     border: 0;
