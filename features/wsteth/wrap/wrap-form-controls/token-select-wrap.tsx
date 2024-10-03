41b1a26258ad3ae909c26dd7157794e91c8686a7
Anton Shalimov <a.shalimof@yandex.ru>
2024-10-04 00:55:01 +0300

    fix: wrap/unwrap options for l2

--- features/wsteth/wrap/wrap-form-controls/token-select-wrap.tsx
+++ features/wsteth/wrap/wrap-form-controls/token-select-wrap.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { trackEvent } from '@lidofinance/analytics-matomo';
 
 import { TOKENS_TO_WRAP } from 'features/wsteth/shared/types';
@@ -5,16 +6,15 @@ import { MATOMO_CLICK_EVENTS } from 'consts/matomo-click-events';
 import { TokenSelectHookForm } from 'shared/hook-form/controls/token-select-hook-form/token-select-hook-form';
 import { useDappStatus } from 'shared/hooks/use-dapp-status';
 
-const OPTIONS = [
-  {
-    label: 'Lido (stETH)',
-    token: TOKENS_TO_WRAP.STETH,
-  },
-  {
-    label: 'Ethereum (ETH)',
-    token: TOKENS_TO_WRAP.ETH,
-  },
-];
+const OPTION_STETH = {
+  label: 'Lido (stETH)',
+  token: TOKENS_TO_WRAP.STETH,
+};
+
+const OPTION_ETH = {
+  label: 'Ethereum (ETH)',
+  token: TOKENS_TO_WRAP.ETH,
+};
 
 type TokenSelectWrapProps = Pick<
   React.ComponentProps<typeof TokenSelectHookForm>,
@@ -22,12 +22,20 @@ type TokenSelectWrapProps = Pick<
 >;
 
 export const TokenSelectWrap = (props: TokenSelectWrapProps) => {
-  const { isWalletConnected, isDappActive } = useDappStatus();
+  const { isWalletConnected, isDappActive, isDappActiveOnL2 } = useDappStatus();
+
+  const options = useMemo(() => {
+    if (isDappActiveOnL2) {
+      return [OPTION_STETH];
+    } else {
+      return [OPTION_STETH, OPTION_ETH];
+    }
+  }, [isDappActiveOnL2]);
 
   return (
     <TokenSelectHookForm
       disabled={isWalletConnected && !isDappActive}
-      options={OPTIONS}
+      options={options}
       onChange={(value) => {
         trackEvent(
           ...(value === TOKENS_TO_WRAP.ETH
