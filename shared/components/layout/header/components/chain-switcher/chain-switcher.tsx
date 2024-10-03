70508723190db37710c43adc8cc230e6790d8fd7
Anton Shalimov <a.shalimof@yandex.ru>
2024-10-04 00:40:00 +0300

    feat: auto change in ChainSwitcher

--- shared/components/layout/header/components/chain-switcher/chain-switcher.tsx
+++ shared/components/layout/header/components/chain-switcher/chain-switcher.tsx
@@ -1,10 +1,20 @@
-import { FC, useState, useCallback, ReactNode } from 'react';
+import {
+  FC,
+  ReactNode,
+  useState,
+  useCallback,
+  useEffect,
+  useMemo,
+} from 'react';
 import { useRouter } from 'next/router';
+import { useAccount } from 'wagmi';
+
 import { Option } from '@lidofinance/lido-ui';
 
 import { ReactComponent as OptimismLogo } from 'assets/icons/chain-toggler/optimism.svg';
 import { ReactComponent as EthereumMainnetLogo } from 'assets/icons/chain-toggler/mainnet.svg';
 
+import { CHAINS } from 'consts/chains';
 import {
   ChainNameType,
   ETHEREUM,
@@ -22,13 +32,36 @@ const iconsMap: Record<ChainNameType, ReactNode> = {
 };
 
 export const ChainSwitcher: FC = () => {
+  const { chainId } = useAccount();
   const { setChainName } = useDappChain();
   const { isDappActiveAndNetworksMatched } = useDappStatus();
   const router = useRouter();
 
   const [value, setValue] = useState<ChainNameType>(ETHEREUM);
 
-  const isOnWrapUnwrapPage = router.pathname === '/wrap/[[...mode]]';
+  const isOnWrapUnwrapPage = useMemo(
+    () => router.pathname === '/wrap/[[...mode]]',
+    [router.pathname],
+  );
+
+  useEffect(() => {
+    if (!chainId) return;
+
+    if ([CHAINS.Mainnet, CHAINS.Holesky, CHAINS.Sepolia].includes(chainId)) {
+      setValue(ETHEREUM);
+      setChainName(ETHEREUM);
+    } else if ([CHAINS.Optimism, CHAINS.OptimismSepolia].includes(chainId)) {
+      setValue(OPTIMISM);
+      setChainName(OPTIMISM);
+    }
+  }, [chainId, setChainName]);
+
+  useEffect(() => {
+    if (!isOnWrapUnwrapPage) {
+      setValue(ETHEREUM);
+      setChainName(ETHEREUM);
+    }
+  }, [isOnWrapUnwrapPage, setChainName]);
 
   const onChange = useCallback(
     (value: any) => {
