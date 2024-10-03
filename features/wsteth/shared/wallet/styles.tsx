c77ab09c93d4f85c8b7fa878de45380527b795b7
Anton Shalimov <a.shalimof@yandex.ru>
2024-10-03 16:57:50 +0300

    feat: wallet block is red for optimism

--- features/wsteth/shared/wallet/styles.tsx
+++ features/wsteth/shared/wallet/styles.tsx
@@ -1,7 +1,13 @@
 import { Card } from 'shared/wallet';
 import styled from 'styled-components';
 
-export const StyledCard = styled(Card)`
-  background: linear-gradient(52.01deg, #1b3349 0%, #25697e 100%);
-  //background: linear-gradient(52.01deg, #37394A 0%, #1D1E35 0.01%, #B73544 100%);
+interface StyledCardProps {
+  $redBg?: boolean;
+}
+
+export const StyledCard = styled(Card)<StyledCardProps>`
+  background: ${({ $redBg }) =>
+    $redBg
+      ? 'linear-gradient(52.01deg, #37394A 0%, #1D1E35 0.01%, #B73544 100%)'
+      : 'linear-gradient(52.01deg, #1b3349 0%, #25697e 100%)'};
 `;
