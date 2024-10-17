f1452231c5d2ccb62a98aed8ac0a17af4915bdb1
Anton Shalimov <a.shalimof@yandex.ru>
2024-10-17 14:52:52 +0300

    feat: pausedInfo for Sepolia

--- features/withdrawals/request/form/paused-info.tsx
+++ features/withdrawals/request/form/paused-info.tsx
@@ -1,13 +1,29 @@
-import { InfoBoxStyled } from 'features/withdrawals/shared';
+import { useAccount } from 'wagmi';
 import { Link } from '@lidofinance/lido-ui';
+
+import { config } from 'config';
+import { CHAINS } from 'consts/chains';
+
+import { InfoBoxStyled } from 'features/withdrawals/shared';
 
 const LIDO_TWITTER_LINK = 'https://twitter.com/lidofinance';
 
 export const PausedInfo = () => {
+  const { chainId } = useAccount();
+
+  let link = <Link href={LIDO_TWITTER_LINK}>see here</Link>;
+
+  if (chainId == CHAINS.Sepolia) {
+    link = (
+      <Link href={`${config.docsOrigin}/deployed-contracts/sepolia/`}>
+        see here
+      </Link>
+    );
+  }
+
   return (
     <InfoBoxStyled>
-      Withdrawals are currently unavailable. For more information,{' '}
-      <Link href={LIDO_TWITTER_LINK}>see here</Link>
+      Withdrawals are currently unavailable. For more information, {link}
     </InfoBoxStyled>
   );
 };
