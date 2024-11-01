a7d2eaf835dd6d3dc5fb6aa1accce9f22a452341
Anton Shalimov <a.shalimof@yandex.ru>
2024-11-01 16:08:54 +0300

    refactor: getEtherscanLink

--- utils/etherscan.ts
+++ utils/etherscan.ts
@@ -1,4 +1,4 @@
-import { mainnet } from 'viem/chains';
+import invariant from 'tiny-invariant';
 import { wagmiChainMap } from 'modules/web3/web3-provider/web3-provider';
 
 export enum ETHERSCAN_ENTITIES {
@@ -12,11 +12,9 @@ export const getEtherscanLink = (
   hash: string,
   entity: ETHERSCAN_ENTITIES,
 ): string => {
-  const wagmiChain = wagmiChainMap[chainId] ?? mainnet;
-  return wagmiChain?.blockExplorers?.default?.url
-    ? `${wagmiChain.blockExplorers.default.url}/${entity}/${hash}`
-    : // Most likely this fallback will never work
-      `https://etherscan1.io/${entity}/${hash}`;
+  const wagmiChain = wagmiChainMap[chainId];
+  invariant(wagmiChain, `chainId ${chainId} is unknown by wagmiChainMap`);
+  return `${wagmiChain?.blockExplorers?.default?.url}/${entity}/${hash}`;
 };
 
 export const getEtherscanTxLink = (chainId: number, hash: string): string => {
