2c75f060e37a62e07a573fd9764e6f3c6c5f73e8
Dmitrii Podlesnyi <rezed93@gmail.com>
2024-11-08 16:04:29 +0700

    feat: unichain fallback banner

--- shared/wallet/fallback/lido-multichain-fallback/lido-multichain-fallback.tsx
+++ shared/wallet/fallback/lido-multichain-fallback/lido-multichain-fallback.tsx
@@ -13,6 +13,7 @@ import { ReactComponent as ScrollLogo } from 'assets/icons/lido-multichain/scrol
 import { ReactComponent as BNBLogo } from 'assets/icons/lido-multichain/bnb.svg';
 import { ReactComponent as ModeLogo } from 'assets/icons/lido-multichain/mode.svg';
 import { ReactComponent as ZircuitLogo } from 'assets/icons/lido-multichain/zircuit-logo.svg';
+import { ReactComponent as UnichainLogo } from 'assets/icons/lido-multichain/unichain.svg';
 
 import { config } from 'config';
 import { LIDO_MULTICHAIN_CHAINS } from 'consts/chains';
@@ -39,6 +40,7 @@ const multichainLogos = {
   [LIDO_MULTICHAIN_CHAINS['BNB Chain']]: BNBLogo,
   [LIDO_MULTICHAIN_CHAINS['Mode Chain']]: ModeLogo,
   [LIDO_MULTICHAIN_CHAINS['Zircuit Chain']]: ZircuitLogo,
+  [LIDO_MULTICHAIN_CHAINS.Unichain]: UnichainLogo,
 };
 
 const getChainLogo = (chainId: LIDO_MULTICHAIN_CHAINS) => {
