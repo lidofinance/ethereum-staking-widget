90488e4ec5b123aa056b0ea33f7656fa9e1ce355
Dmitriy Evtifeev <evtifeev.dmitry@gmail.com>
2024-11-12 19:57:39 +0300

    feat: updated check external forbidden route function to provider

--- shared/components/layout/layout.tsx
+++ shared/components/layout/layout.tsx
@@ -1,14 +1,8 @@
 import { ReactNode, FC, PropsWithChildren } from 'react';
-
 import { ContainerProps } from '@lidofinance/lido-ui';
-import { useRouter } from 'next/router';
-import { useRouterPath } from 'shared/hooks/use-router-path';
 
-import { config, useConfig } from 'config';
-import { ManifestConfigPage } from 'config/external-config';
-import { HOME_PATH } from 'consts/urls';
+import { config } from 'config';
 
-import { LayoutEffectSsrDelayed } from 'shared/components/layout-effect-ssr-delayed';
 import { IPFSInfoBox } from 'features/ipfs/ipfs-info-box';
 import { Header } from './header/header';
 import { Footer } from './footer/footer';
@@ -18,6 +12,7 @@ import {
   LayoutSubTitleStyle,
   IPFSInfoBoxOnlyMobileAndPortableWrapper,
 } from './styles';
+// import { ExternalForbiddenPage } from '../external-forbidden-page';
 
 type Props = {
   title?: ReactNode;
@@ -28,24 +23,11 @@ type Props = {
 export const Layout: FC<PropsWithChildren<Props>> = (props) => {
   const { title, subtitle, containerSize } = props;
   const { children } = props;
-  const router = useRouter();
-  const path = useRouterPath();
-  const { pages } = useConfig().externalConfig;
-
-  const checkPathEffect = () => {
-    if (pages) {
-      const paths = Object.keys(pages) as ManifestConfigPage[];
-      const forbiddenPath = paths.find((pathKey) => path.includes(pathKey));
-      if (forbiddenPath && pages[forbiddenPath]?.deactivate) {
-        void router.push(HOME_PATH);
-      }
-    }
-  };
 
   return (
     <>
       <Header />
-      <LayoutEffectSsrDelayed effect={checkPathEffect} deps={[pages]} />
+      {/*<ExternalForbiddenPage />*/}
       <Main size={containerSize}>
         {config.ipfsMode && (
           <IPFSInfoBoxOnlyMobileAndPortableWrapper>
