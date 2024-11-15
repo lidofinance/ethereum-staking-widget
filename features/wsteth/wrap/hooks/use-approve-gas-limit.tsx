1be65962ea911f2203b3eba48261112dac5e257a
Anton Shalimov <a.shalimof@yandex.ru>
2024-11-15 17:56:10 +0300

    refactor(useApproveGasLimit): migrate from useLidoSWR to useLidoQuery

--- features/wsteth/wrap/hooks/use-approve-gas-limit.tsx
+++ features/wsteth/wrap/hooks/use-approve-gas-limit.tsx
@@ -1,11 +1,11 @@
-import { useLidoSWR } from '@lido-sdk/react';
-
 import { config } from 'config';
 import {
   STETH_L2_APPROVE_GAS_LIMIT,
   WSTETH_APPROVE_GAS_LIMIT,
 } from 'consts/tx';
-import { STRATEGY_LAZY } from 'consts/swr-strategies';
+import { STRATEGY_LAZY } from 'consts/react-query-strategies';
+
+import { useLidoQuery } from 'shared/hooks/use-lido-query';
 import { useLidoSDK, useDappStatus } from 'modules/web3';
 
 export const useApproveGasLimit = () => {
@@ -16,22 +16,19 @@ export const useApproveGasLimit = () => {
     ? STETH_L2_APPROVE_GAS_LIMIT
     : WSTETH_APPROVE_GAS_LIMIT;
 
-  // TODO: NEW_SDK (useQuery)
-  const { data } = useLidoSWR(
-    ['swr:approve-wrap-gas-limit', isDappActiveOnL2, core.chainId],
-    async () => {
+  const { data } = useLidoQuery<bigint | undefined>({
+    queryKey: ['approve-wrap-gas-limit', isDappActiveOnL2, core.chainId],
+    strategy: STRATEGY_LAZY,
+    queryFn: async () => {
       try {
-        // wsteth on l1, steth on l2
         const spender = await (isL2
           ? l2.contractAddress()
           : wstETH.contractAddress());
 
-        // steth on l1, wsteth on l2
         const contract = await (isL2 ? l2.getContract() : stETH.getContract());
 
         return await contract.estimateGas.approve(
-          // TODO: NEW_SDK (ESTIMATE_AMOUNT will be bigint)
-          [spender, config.ESTIMATE_AMOUNT.toBigInt()],
+          [spender, config.ESTIMATE_AMOUNT_BIGINT],
           {
             account: config.ESTIMATE_ACCOUNT,
           },
@@ -41,8 +38,7 @@ export const useApproveGasLimit = () => {
         return fallback;
       }
     },
-    STRATEGY_LAZY,
-  );
+  });
 
   return data ?? fallback;
 };
