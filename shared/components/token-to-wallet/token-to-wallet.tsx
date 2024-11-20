a61d0706072ce46a518b8dfcd005add88dccce04
Dmitrii Podlesnyi <rezed93@gmail.com>
2024-11-20 22:51:58 +0900

    feat: more token to wallet response messages

--- shared/components/token-to-wallet/token-to-wallet.tsx
+++ shared/components/token-to-wallet/token-to-wallet.tsx
@@ -1,10 +1,9 @@
-import { ToastInfo, Tooltip } from '@lidofinance/lido-ui';
+import { ToastError, ToastInfo, Tooltip } from '@lidofinance/lido-ui';
 import { TokenToWalletStyle } from './styles';
 
 import { Component } from 'types';
 import { useWalletClient, useWatchAsset } from 'wagmi';
 import { Address, getContract } from 'viem';
-import { useConnectorInfo } from 'reef-knot/core-react';
 
 export type TokenToWalletComponent = Component<'button', { address?: string }>;
 
@@ -27,10 +26,9 @@ const ERC20_METADATA_ABI = [
 
 export const TokenToWallet: TokenToWalletComponent = ({ address, ...rest }) => {
   const { watchAssetAsync } = useWatchAsset();
-  const { isInjected } = useConnectorInfo();
   const { data: walletClient } = useWalletClient();
 
-  if (!walletClient || !address || !isInjected) return null;
+  if (!walletClient || !address) return null;
 
   const onClickHandler = async () => {
     if (!address) return;
@@ -51,10 +49,36 @@ export const TokenToWallet: TokenToWalletComponent = ({ address, ...rest }) => {
         type: 'ERC20',
         options: { address, decimals, symbol },
       });
-      if (!result) return;
 
-      ToastInfo('Tokens were successfully added to your wallet', {});
+      if (result) {
+        ToastInfo('Tokens were successfully added to your wallet', {});
+      } else {
+        ToastInfo('User rejected the request');
+      }
     } catch (error) {
+      // Associating error code to UI messages
+      if (error && typeof error === 'object' && 'code' in error) {
+        if (
+          error?.code === -32602 // Trust
+        ) {
+          ToastInfo('Tokens already existed');
+        } else if (
+          error?.code === 4001 || // Metamask, coin98, okx
+          error?.code === -32603 // Bitget
+        ) {
+          ToastInfo('User rejected the request');
+        } else if (
+          error?.code === -1 || // LL and Safe through WC
+          error?.code === -32601 // LL in Discover
+        ) {
+          ToastError('The wallet does not support adding a token');
+        } else {
+          ToastError(
+            'An error occurred while adding token to wallet\nThe wallet probably does not support adding a token',
+          );
+        }
+      }
+
       console.warn('[TokenToWallet] error adding token to wallet', error);
     }
   };
