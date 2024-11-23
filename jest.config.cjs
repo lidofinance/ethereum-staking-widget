138cd35155b3832b2198a9d9932283009d72284e
Anton Shalimov <a.shalimof@yandex.ru>
2024-11-23 13:25:36 +0300

    fix(jest tests): wagmi import

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
+  testEnvironment: 'node',
   transform: {
     '^.+\\.(t|j)sx?$': 'ts-jest',
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!(wagmi|viem|@tanstack/react-query)/)',
+  ],
   moduleDirectories: ['node_modules', '<rootDir>'],
   modulePathIgnorePatterns: ['./test'],
 };
