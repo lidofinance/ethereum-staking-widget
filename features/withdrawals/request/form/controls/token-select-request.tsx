caffc90a9ca3fef035cf205a1731078c4c85527b
Anton Shalimov <a.shalimof@yandex.ru>
2024-11-23 14:23:45 +0300

    fix: type

--- features/withdrawals/request/form/controls/token-select-request.tsx
+++ features/withdrawals/request/form/controls/token-select-request.tsx
@@ -3,11 +3,11 @@ import {
   TokenSelectHookForm,
 } from 'shared/hook-form/controls/token-select-hook-form/token-select-hook-form';
 import { useDappStatus } from 'modules/web3';
-import { TOKENS_TO_WRAP } from '../../../../wsteth/shared/types';
+import { TOKENS_TO_WITHDRAWLS } from '../../../types/tokens-withdrawable';
 
 const OPTIONS: TokenOption[] = [
-  { token: TOKENS_TO_WRAP.stETH },
-  { token: TOKENS_TO_WRAP.wstETH },
+  { token: TOKENS_TO_WITHDRAWLS.stETH },
+  { token: TOKENS_TO_WITHDRAWLS.wstETH },
 ];
 
 export const TokenSelectRequest = () => {
