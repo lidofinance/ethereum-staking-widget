25e98d89259571e1f0f0985b9530d2372d1f898c
Anton Shalimov <a.shalimof@yandex.ru>
2024-11-25 00:09:40 +0300

    refactor(useEthUsd): use readContract

--- shared/hooks/use-eth-usd.ts
+++ shared/hooks/use-eth-usd.ts
@@ -1,22 +1,12 @@
 import { useMemo } from 'react';
 import type { Address } from 'viem';
-import { encodeFunctionData, decodeAbiParameters } from 'viem';
 import { usePublicClient } from 'wagmi';
 import { CHAINS } from '@lidofinance/lido-ethereum-sdk';
 import { useQuery } from '@tanstack/react-query';
 
+import { AggregatorAbi } from 'abi/aggregator-abi';
 import { AGGREGATOR_STETH_USD_PRICE_FEED_BY_NETWORK } from 'consts/aggregator';
 import { STRATEGY_LAZY } from 'consts/react-query-strategies';
-
-const aggregatorAbi = [
-  {
-    inputs: [],
-    name: 'latestAnswer',
-    outputs: [{ internalType: 'int256', name: '', type: 'int256' }],
-    stateMutability: 'view',
-    type: 'function',
-  },
-];
 
 export const useEthUsd = (amount?: bigint) => {
   const publicClientMainnet = usePublicClient({ chainId: CHAINS.Mainnet });
@@ -34,20 +24,13 @@ export const useEthUsd = (amount?: bigint) => {
     queryFn: async () => {
       if (!publicClientMainnet) return;
 
-      const callData = encodeFunctionData({
-        abi: aggregatorAbi,
+      const latestAnswer = await publicClientMainnet.readContract({
+        address: AGGREGATOR_STETH_USD_PRICE_FEED_BY_NETWORK[
+          CHAINS.Mainnet
+        ] as Address,
+        abi: AggregatorAbi,
         functionName: 'latestAnswer',
       });
-
-      const result = await publicClientMainnet.call({
-        to: AGGREGATOR_STETH_USD_PRICE_FEED_BY_NETWORK[CHAINS.Mainnet],
-        data: callData,
-      });
-
-      const [latestAnswer] = decodeAbiParameters(
-        [{ internalType: 'int256', name: '', type: 'int256' }],
-        result.data as Address, // view returns (uint256)
-      );
 
       return latestAnswer / BigInt(10 ** 8);
     },
