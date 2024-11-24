0953e119d36563e731bdad1701f0b25c4670d0aa
Anton Shalimov <a.shalimof@yandex.ru>
2024-11-24 23:27:04 +0300

    refactor(useStethEthRate): use useReadContract

--- features/rewards/hooks/use-steth-eth-rate.ts
+++ features/rewards/hooks/use-steth-eth-rate.ts
@@ -1,10 +1,7 @@
-import type { Address } from 'viem';
-import { parseAbiItem, encodeFunctionData, decodeAbiParameters } from 'viem';
-import { usePublicClient } from 'wagmi';
 import { CHAINS } from '@lidofinance/lido-ethereum-sdk';
-import { useQuery } from '@tanstack/react-query';
+import { useReadContract, usePublicClient } from 'wagmi';
 
-import { STRATEGY_LAZY } from 'consts/react-query-strategies';
+import { PartialCurveAbi } from 'abi/partial-curve-abi';
 import { WEI_PER_ETHER } from 'consts/tx';
 import { useDappStatus } from 'modules/web3';
 
@@ -14,34 +11,13 @@ export const useStethEthRate = () => {
   const { chainId } = useDappStatus();
   const publicClientMainnet = usePublicClient({ chainId: CHAINS.Mainnet });
 
-  const { data, error } = useQuery({
-    queryKey: ['steth-eth-rate', chainId],
-    ...STRATEGY_LAZY,
-    enabled: !!(chainId === CHAINS.Mainnet && publicClientMainnet),
-    queryFn: async () => {
-      if (chainId !== 1 || !publicClientMainnet) return WEI_PER_ETHER;
-
-      const functionSignature = parseAbiItem(
-        'function get_dy(int128 i, int128 j, uint256 dx) view returns (uint256)',
-      );
-
-      const callData = encodeFunctionData({
-        abi: [functionSignature],
-        functionName: 'get_dy',
-        args: [BigInt(0), BigInt(1), WEI_PER_ETHER],
-      });
-
-      const result = await publicClientMainnet.call({
-        to: MAINNET_CURVE,
-        data: callData,
-      });
-
-      const decoded = decodeAbiParameters(
-        functionSignature.outputs || [],
-        result.data as Address, // view returns (uint256)
-      );
-
-      return decoded[0];
+  const { data, error } = useReadContract({
+    address: MAINNET_CURVE,
+    abi: PartialCurveAbi,
+    functionName: 'get_dy',
+    args: [0n, 1n, WEI_PER_ETHER],
+    query: {
+      enabled: chainId === CHAINS.Mainnet && !!publicClientMainnet,
     },
   });
 
