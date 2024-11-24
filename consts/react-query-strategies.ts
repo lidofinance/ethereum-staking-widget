812c3e77a1103d94ae54f5492ac4492c307e09d0
Anton Shalimov <a.shalimof@yandex.ru>
2024-11-24 15:25:01 +0300

    refactor: remove cacheTime from strategies

--- consts/react-query-strategies.ts
+++ consts/react-query-strategies.ts
@@ -1,13 +1,11 @@
 export const STRATEGY_IMMUTABLE = {
   staleTime: Infinity,
-  cacheTime: Infinity,
   refetchOnWindowFocus: false,
   refetchOnReconnect: false,
 };
 
 export const STRATEGY_CONSTANT = {
   staleTime: Infinity,
-  cacheTime: Infinity,
   refetchOnWindowFocus: false,
   refetchOnReconnect: false,
   refetchInterval: 10 * 60 * 1000, // 10 minutes
@@ -15,14 +13,12 @@ export const STRATEGY_CONSTANT = {
 
 export const STRATEGY_LAZY = {
   staleTime: 5 * 60 * 1000, // 5 minutes
-  cacheTime: Infinity,
   refetchOnWindowFocus: false,
   refetchOnReconnect: true,
 };
 
 export const STRATEGY_EAGER = {
   staleTime: 3000, // 3 seconds
-  cacheTime: Infinity,
   refetchOnWindowFocus: true,
   refetchOnReconnect: true,
   refetchInterval: 10000, // 10 seconds
