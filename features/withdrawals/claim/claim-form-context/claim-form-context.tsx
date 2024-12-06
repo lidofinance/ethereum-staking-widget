a723a99a7d92724b37770d1584e9a54a4aebcf9f
Dmitrii Podlesnyi <rezed93@gmail.com>
2024-12-06 15:48:52 +0700

    refactor: claim page prev checked state typecheck simplified

--- features/withdrawals/claim/claim-form-context/claim-form-context.tsx
+++ features/withdrawals/claim/claim-form-context/claim-form-context.tsx
@@ -80,7 +80,7 @@ export const ClaimFormProvider: FC<PropsWithChildren> = ({ children }) => {
     if (!data || isSubmitting) return;
 
     // for regular updates generate new list but keep user input
-    const prevCheckState = getValues('requests')?.reduce(
+    const prevCheckState = (getValues('requests') || {}).reduce(
       (res, req) => ({
         ...res,
         [req.token_id]: req.checked,
@@ -94,8 +94,8 @@ export const ClaimFormProvider: FC<PropsWithChildren> = ({ children }) => {
     ).requests.map((request) => ({
       ...request,
       checked:
-        prevCheckState?.[request.token_id] !== undefined
-          ? prevCheckState?.[request.token_id]
+        prevCheckState[request.token_id] !== undefined
+          ? prevCheckState[request.token_id]
           : request.status.isFinalized,
     }));
 
