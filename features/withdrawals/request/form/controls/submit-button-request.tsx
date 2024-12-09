cf3310f4bc7c7b6f4cf63d0f4186b68e39d9c916
Evgeny Taktarov <je.mouth@gmail.com>
2024-12-09 20:28:03 +0700

    fix: remove redun check

--- features/withdrawals/request/form/controls/submit-button-request.tsx
+++ features/withdrawals/request/form/controls/submit-button-request.tsx
@@ -7,7 +7,6 @@ import {
 import { SubmitButtonHookForm } from 'shared/hook-form/controls/submit-button-hook-form';
 import { useFormState } from 'react-hook-form';
 import { isValidationErrorTypeUnhandled } from 'shared/hook-form/validation/validation-error';
-import { useAA, useDappStatus } from 'modules/web3';
 import { DisabledButton } from 'shared/wallet';
 
 // conditional render breaks useFormState, so it can't be inside SubmitButton
@@ -34,12 +33,9 @@ export const SubmitButtonRequest = ({
   disabled,
 }: SubmitButtonRequestProps) => {
   const { isSupportedChain } = useDappStatus();
-  const { isAA } = useAA();
   const { isTokenLocked } = useRequestFormData();
 
-  const isLocked = isTokenLocked && !isAA;
-
-  const buttonTitle = isLocked
+  const buttonTitle = isTokenLocked
     ? `Unlock tokens and withdraw`
     : 'Request withdrawal';
 
@@ -51,7 +47,7 @@ export const SubmitButtonRequest = ({
     <SubmitButtonHookForm
       errorField="amount"
       loading={loading}
-      isLocked={isLocked}
+      isLocked={isTokenLocked}
       disabled={disabled}
       data-testid="requestButton"
     >
