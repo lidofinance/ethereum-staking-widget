a97965ffa5cf431d8a22845a05b1a9bab9e7abc0
Anton Shalimov <a.shalimof@yandex.ru>
2025-01-13 13:19:13 +0300

    feat(useAppChainManager): add useMemo

--- modules/web3/web3-provider/use-app-chain-manager.ts
+++ modules/web3/web3-provider/use-app-chain-manager.ts
@@ -1,4 +1,10 @@
-import { useState, useEffect, useCallback } from 'react';
+import {
+  useState,
+  useEffect,
+  useCallback,
+  useMemo,
+  type Dispatch,
+} from 'react';
 import { useAccount, useSwitchChain } from 'wagmi';
 
 import { config } from 'config';
@@ -23,8 +29,12 @@ export const useAppChainManager = (supportedL2: boolean) => {
     },
   });
 
-  const supportedChainIds = config.supportedChains.filter((chain) =>
-    supportedL2 ? true : !isSDKSupportedL2Chain(chain),
+  const supportedChainIds = useMemo(
+    () =>
+      config.supportedChains.filter((chain) =>
+        supportedL2 ? true : !isSDKSupportedL2Chain(chain),
+      ),
+    [supportedL2],
   );
 
   // Sync 'app chain id' with the 'wallet chain id' or use default
@@ -39,7 +49,7 @@ export const useAppChainManager = (supportedL2: boolean) => {
     }
   }, [walletChainId, isConnected, supportedChainIds]);
 
-  const switchAppChainId = useCallback<React.Dispatch<number>>(
+  const switchAppChainId = useCallback<Dispatch<number>>(
     (newChainId: number) => {
       if (supportedChainIds.includes(newChainId)) {
         switchChain({ chainId: newChainId });
@@ -48,39 +58,48 @@ export const useAppChainManager = (supportedL2: boolean) => {
     [switchChain, supportedChainIds],
   );
 
-  const isChainIdOnL2 =
-    supportedL2 &&
-    getChainTypeByChainId(appChainId) === DAPP_CHAIN_TYPE.Optimism;
+  const [isChainIdOnL2, supportedChainLabels] = useMemo(() => {
+    const isChainIdOnL2 =
+      supportedL2 &&
+      getChainTypeByChainId(appChainId) === DAPP_CHAIN_TYPE.Optimism;
 
-  const supportedChainTypes = supportedChainIds
-    .map(getChainTypeByChainId)
-    .filter(
-      (chainType, index, array) =>
-        // duplicate/invalid pruning + stable order
-        chainType && array.indexOf(chainType) === index,
-    ) as DAPP_CHAIN_TYPE[];
+    const supportedChainTypes = supportedChainIds
+      .map(getChainTypeByChainId)
+      .filter(
+        (chainType, index, array) =>
+          // duplicate/invalid pruning + stable order
+          chainType && array.indexOf(chainType) === index,
+      ) as DAPP_CHAIN_TYPE[];
 
-  const getChainLabelByType = (chainType: DAPP_CHAIN_TYPE) => {
-    // all testnets for chainType
-    const testnetsForType = supportedChainIds
-      .filter((id) => chainType == getChainTypeByChainId(id))
-      .map((id) => wagmiChainMap[id])
-      .filter((chain) => chain.testnet)
-      .map((chain) => chain.name);
+    const getChainLabelByType = (chainType: DAPP_CHAIN_TYPE) => {
+      // all testnets for chainType
+      const testnetsForType = supportedChainIds
+        .filter((id) => chainType == getChainTypeByChainId(id))
+        .map((id) => wagmiChainMap[id])
+        .filter((chain) => chain.testnet)
+        .map((chain) => chain.name);
 
-    return (
-      chainType +
-      (testnetsForType.length > 0 ? `(${testnetsForType.join(',')})` : '')
-    );
-  };
+      return (
+        chainType +
+        (testnetsForType.length > 0 ? `(${testnetsForType.join(',')})` : '')
+      );
+    };
 
-  const supportedChainLabels = supportedChainTypes.reduce(
-    (acc, chainType) => ({
-      ...acc,
-      [chainType]: getChainLabelByType(chainType),
-    }),
-    {},
-  ) as SupportedChainLabels;
+    const supportedChainLabels = supportedChainTypes.reduce(
+      (acc, chainType) => ({
+        ...acc,
+        [chainType]: getChainLabelByType(chainType),
+      }),
+      {},
+    ) as SupportedChainLabels;
+
+    return [isChainIdOnL2, supportedChainLabels];
+  }, [supportedL2, appChainId, supportedChainIds]);
+
+  const isSupportedChain = useMemo(
+    () => (walletChainId ? supportedChainIds.includes(walletChainId) : true),
+    [walletChainId, supportedChainIds],
+  );
 
   return {
     chainId: appChainId,
@@ -92,9 +111,7 @@ export const useAppChainManager = (supportedL2: boolean) => {
 
     supportedChainLabels,
     supportedChainIds,
-    isSupportedChain: walletChainId
-      ? supportedChainIds.includes(walletChainId)
-      : true,
+    isSupportedChain,
     isChainIdMatched: walletChainId === appChainId,
   };
 };
