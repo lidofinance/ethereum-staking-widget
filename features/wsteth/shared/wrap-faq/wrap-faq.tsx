4ef8b389c2ad53bd259a68302619976ed9d4ddc4
alx-khramov <alx.khramov@gmail.com>
2025-01-27 16:57:55 +0300

    feat: add Soneium FAQ
    Signed-off-by: alx-khramov <alx.khramov@gmail.com>

--- features/wsteth/shared/wrap-faq/wrap-faq.tsx
+++ features/wsteth/shared/wrap-faq/wrap-faq.tsx
@@ -7,6 +7,7 @@ import { useMatomoEventHandle } from 'shared/hooks';
 
 import { EthereumFAQ } from './ethereum-faq/faq';
 import { OptimismFAQ } from './optimism-faq/faq';
+import { SoneiumFAQ } from './soneium-faq/faq';
 
 export const faqComponentsMap = new Map([
   [CHAINS.Mainnet, EthereumFAQ],
@@ -14,6 +15,8 @@ export const faqComponentsMap = new Map([
   [CHAINS.Holesky, EthereumFAQ],
   [CHAINS.Optimism, OptimismFAQ],
   [CHAINS.OptimismSepolia, OptimismFAQ],
+  [CHAINS.Soneium, SoneiumFAQ],
+  [CHAINS.SoneiumMinato, SoneiumFAQ],
 ]);
 
 export const WrapFaq = () => {
