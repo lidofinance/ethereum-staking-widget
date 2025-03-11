1ab03dd7d89af1be671ed16a15fe546bdb900f27
alx-khramov <alx.khramov@gmail.com>
2025-03-11 12:07:04 +0300

    refactor: improve fallback handling in getRefferalAddress function
    Signed-off-by: alx-khramov <alx.khramov@gmail.com>

--- features/stake/stake-form/utils.ts
+++ features/stake/stake-form/utils.ts
@@ -9,23 +9,23 @@ export const getRefferalAddress = async (
   try {
     const fallback = config.STAKE_FALLBACK_REFERRAL_ADDRESS;
 
-    if (input) {
-      // The address is Ethereum address
-      if (isAddress(input)) return input;
+    if (!input) return fallback;
 
-      // Not all ENS names end with .eth, so we can't detect ENS names easily.
-      // The address is a http[s] link, return fallback instead
-      if (/^https?:\/\//.test(input)) return fallback;
-      // Filter out *.lido.fi referrals, e.g. ref from blog.lido.fi
-      // Assuming, that no one uses the 'lido.fi' ENS name
-      if (input.endsWith('lido.fi')) return fallback;
+    // The address is Ethereum address
+    if (isAddress(input)) return input;
 
-      // Trying to get ENS address takes some time, so it is called after another checks
-      const ensAddress = await getEnsAddress(provider, {
-        name: normalize(input),
-      });
-      if (ensAddress) return ensAddress.toString();
-    }
+    // Not all ENS names end with .eth, so we can't detect ENS names easily.
+    // The address is a http[s] link, return fallback instead
+    if (/^https?:\/\//.test(input)) return fallback;
+    // Filter out *.lido.fi referrals, e.g. ref from blog.lido.fi
+    // Assuming, that no one uses the 'lido.fi' ENS name
+    if (input.endsWith('lido.fi')) return fallback;
+
+    // Trying to get ENS address takes some time, so it is called after another checks
+    const ensAddress = await getEnsAddress(provider, {
+      name: normalize(input),
+    });
+    if (ensAddress) return ensAddress.toString();
 
     // the provided 'input' is not an Ethereum address, nor a ENS address, returning the fallback
     return fallback;
