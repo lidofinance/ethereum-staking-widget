4da126966b7912dc6d34512f9f70c79bf71e3247
Alexander Khramov <alx.khramov@gmail.com>
2025-05-13 14:52:35 +0300

    fix: useDVstEthApr – use select
    Signed-off-by: Alexander Khramov <alx.khramov@gmail.com>

--- shared/hooks/useDVstEthApr.ts
+++ shared/hooks/useDVstEthApr.ts
@@ -2,24 +2,31 @@ import { useQuery } from '@tanstack/react-query';
 import { STRATEGY_LAZY } from 'consts/react-query-strategies';
 import { standardFetcher } from 'utils/standardFetcher';
 
-type API_RESPONSE = Array<Record<string, any>>;
+type VaultDataPartial = {
+  id: string;
+  apr: number;
+};
+type RequestResponseData = Array<VaultDataPartial>;
 
 const API_ENDPOINT = 'https://points.mellow.finance/v1/vaults';
 
 export const useDVstEthApr = () => {
-  const result = useQuery<API_RESPONSE>({
+  const result = useQuery<
+    RequestResponseData,
+    Error,
+    VaultDataPartial | undefined
+  >({
     queryKey: ['dvsteth-apr'],
     ...STRATEGY_LAZY,
     queryFn: async () => {
-      return await standardFetcher<API_RESPONSE>(API_ENDPOINT);
+      return await standardFetcher<RequestResponseData>(API_ENDPOINT);
+    },
+    select: (data) => {
+      return data.find((vault) => vault.id === 'ethereum-dvsteth');
     },
   });
 
-  const dvstethVaultData = result.data?.filter(
-    (vault) => vault.id === 'ethereum-dvsteth',
-  )[0];
-
-  const apr = dvstethVaultData?.apr.toFixed(1);
+  const apr = result.data?.apr.toFixed(1);
 
   return {
     ...result,
