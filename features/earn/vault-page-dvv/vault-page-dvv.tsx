46a8b0dc22cc4906d7fbdc510c797476af2662b6
Alexander Khramov <alx.khramov@gmail.com>
2025-08-09 03:16:06 +0300

    feat(earn): enhance VaultPageDVV and VaultPageGGV components with detailed stats and descriptions
    Signed-off-by: Alexander Khramov <alx.khramov@gmail.com>

--- features/earn/vault-page-dvv/vault-page-dvv.tsx
+++ features/earn/vault-page-dvv/vault-page-dvv.tsx
@@ -1,7 +1,32 @@
 import { FC } from 'react';
+import { Block } from '@lidofinance/lido-ui';
+import { PartnerStakehouseIcon, VaultDDVIcon } from '../shared/icons';
+import { VaultDescription } from '../shared/vault-description';
+import { VaultHeader } from '../shared/vault-header';
+import { VaultStats } from '../shared/vault-stats';
+
+const partners = [
+  {
+    role: 'Curated by',
+    icon: <PartnerStakehouseIcon />,
+    text: 'Stakehouse Financial',
+  },
+];
+const description =
+  'The Decentralized Validator Vault accepts ETH deposits to the Lido protocol, accelerating the adoption of Distributed Validator Technology (DVT)';
+const stats = { tvl: '86', apy: '4.4' };
 
 export const VaultPageDVV: FC<{ action: 'deposit' | 'withdraw' }> = ({
   action,
-}) => {
-  return <>DVV Vault - {action}</>;
-};
+}) => (
+  <Block>
+    <VaultHeader
+      title={`Lido DVV`}
+      logo={<VaultDDVIcon />}
+      partners={partners}
+    />
+    {stats && <VaultStats tvl={stats.tvl} apy={stats.apy} />}
+    <VaultDescription description={description} />
+    {action}
+  </Block>
+);
--- features/earn/vault-page-ggv/vault-page-ggv.tsx
+++ features/earn/vault-page-ggv/vault-page-ggv.tsx
@@ -1,7 +1,37 @@
 import { FC } from 'react';
+import { Block } from '@lidofinance/lido-ui';
+import { VaultHeader } from '../shared/vault-header';
+import {
+  Partner7seasIcon,
+  PartnerVedaIcon,
+  VaultGGVIcon,
+} from '../shared/icons';
+import { VaultDescription } from '../shared/vault-description';
+import { VaultStats } from '../shared/vault-stats';
+
+const partners = [
+  { role: 'Curated by', icon: <Partner7seasIcon />, text: '7seas' },
+  {
+    role: 'Infrastructure provider',
+    icon: <PartnerVedaIcon />,
+    text: 'Veda',
+  },
+];
+const description =
+  'Lido GGV leverages top DeFi protocols to maximize rewards on your stETH, with a single deposit.';
+const stats = { tvl: '431', apy: '10.4' };
 
 export const VaultPageGGV: FC<{ action: 'deposit' | 'withdraw' }> = ({
   action,
-}) => {
-  return <>GGV Vault - {action}</>;
-};
+}) => (
+  <Block>
+    <VaultHeader
+      title={`Lido GGV`}
+      logo={<VaultGGVIcon />}
+      partners={partners}
+    />
+    {stats && <VaultStats tvl={stats.tvl} apy={stats.apy} />}
+    <VaultDescription description={description} />
+    {action}
+  </Block>
+);
