6748c1f236de49cd25c2469313e82112e9a5fccd
Alexander Khramov <alx.khramov@gmail.com>
2025-08-10 01:13:04 +0300

    feat: add fullwidth support to Switch component
    Signed-off-by: Alexander Khramov <alx.khramov@gmail.com>

--- shared/components/switch/switch.tsx
+++ shared/components/switch/switch.tsx
@@ -5,11 +5,11 @@ import { SwitchWrapper, Handle } from './styles';
 import { SwitchProps } from './types';
 
 export const Switch: FC<SwitchProps> = (props) => {
-  const { checked, routes } = props;
+  const { checked, routes, fullwidth, className } = props;
 
   return (
-    <SwitchWrapper>
-      <Handle $checked={checked} />
+    <SwitchWrapper className={className} $fullwidth={fullwidth}>
+      <Handle $checked={checked} $fullwidth={fullwidth} />
       <SwitchItem href={routes[0].path}>{routes[0].name}</SwitchItem>
       <SwitchItem href={routes[1].path}>{routes[1].name}</SwitchItem>
     </SwitchWrapper>
