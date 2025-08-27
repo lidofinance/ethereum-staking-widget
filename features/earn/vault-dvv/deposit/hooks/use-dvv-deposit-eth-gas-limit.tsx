f84f747f3a7156ec60dfd2df774fa32c08413f83
Evgeny Taktarov <je.mouth@gmail.com>
2025-08-27 18:50:44 +0200

    fix: vault estimate gas

--- features/earn/vault-dvv/deposit/hooks/use-dvv-deposit-eth-gas-limit.tsx
+++ features/earn/vault-dvv/deposit/hooks/use-dvv-deposit-eth-gas-limit.tsx
@@ -35,7 +35,7 @@ export const useDVVDepositEthGasLimit = () => {
       }),
       value: ESTIMATE_AMOUNT,
       to: wrapper.address,
-      from: ESTIMATE_ACCOUNT,
+      account: ESTIMATE_ACCOUNT,
     };
   }, [publicClientMainnet]);
 
