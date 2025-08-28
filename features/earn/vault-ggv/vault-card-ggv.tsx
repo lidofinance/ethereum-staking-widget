8efd04670486df37c187c0acb65174a826d6e7bc
Alexander Khramov <alx.khramov@gmail.com>
2025-08-28 15:16:03 +0300

    feat(earn): upd ggv-card apy hint
    Signed-off-by: Alexander Khramov <alx.khramov@gmail.com>

--- features/earn/vault-ggv/vault-card-ggv.tsx
+++ features/earn/vault-ggv/vault-card-ggv.tsx
@@ -8,7 +8,7 @@ import {
 
 import { useDappStatus } from 'modules/web3';
 
-import { EARN_VAULT_GGV_SLUG } from '../consts';
+import { EARN_VAULT_DEPOSIT_SLUG, EARN_VAULT_GGV_SLUG } from '../consts';
 import { VaultCard } from '../shared/vault-card';
 import { trackMatomoEvent } from 'utils/track-matomo-event';
 import { MATOMO_EARN_EVENTS_TYPES } from 'consts/matomo/matomo-earn-events';
@@ -17,6 +17,29 @@ import { useGGVStats } from './hooks/use-ggv-stats';
 import { useGGVPosition } from './hooks/use-ggv-position';
 
 import { GGV_PARTNERS, GGV_TOKEN_SYMBOL } from './consts';
+import { Link } from '@lidofinance/lido-ui';
+import { EARN_PATH } from 'consts/urls';
+
+const ApxHint = () => {
+  const link = `${EARN_PATH}/${EARN_VAULT_GGV_SLUG}/${EARN_VAULT_DEPOSIT_SLUG}#what-is-apy-for-ggv`;
+  return (
+    <span>
+      24-hours average APY after{' '}
+      <Link target="_self" href={link}>
+        fees
+      </Link>
+      <br />
+      <br />
+      APY is the annual percentage yield including compounding Learn more in
+      Lido GGV FAQ
+      <br />
+      <br />
+      <Link target="_self" href={link}>
+        Learn more in Lido GGV FAQ
+      </Link>
+    </span>
+  );
+};
 
 export const VaultCardGGV = () => {
   const { isWalletConnected } = useDappStatus();
@@ -43,7 +66,13 @@ export const VaultCardGGV = () => {
             }
           : undefined
       }
-      stats={{ tvl, apx: apy, apxLabel: 'APY', isLoading: isLoadingStats }}
+      stats={{
+        tvl,
+        apx: apy,
+        apxLabel: 'APY',
+        isLoading: isLoadingStats,
+        apxHint: <ApxHint />,
+      }}
       logo={<VaultGGVIcon />}
       depositLinkCallback={() => {
         trackMatomoEvent(MATOMO_EARN_EVENTS_TYPES.ggvDeposit);
