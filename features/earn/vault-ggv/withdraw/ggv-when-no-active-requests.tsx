5f1c691a58ce71a258947bbdcf0156667f728744
Evgeny Taktarov <je.mouth@gmail.com>
2025-08-28 13:02:16 +0200

    fix: empty ggv withdraw

--- features/earn/vault-ggv/withdraw/ggv-when-no-active-requests.tsx
+++ features/earn/vault-ggv/withdraw/ggv-when-no-active-requests.tsx
@@ -1,11 +1,14 @@
+import { useGGVAvailable } from '../hooks/use-ggv-available';
 import { useGGVWithdrawForm } from './form-context';
 
 export const GGVWhenNoActiveRequests = ({
   children,
 }: React.PropsWithChildren) => {
+  const { isGGVAvailable } = useGGVAvailable();
   const { hasActiveRequests } = useGGVWithdrawForm();
 
-  if (hasActiveRequests) return null;
+  // shows disabled form on wrong chain
+  if (hasActiveRequests && isGGVAvailable) return null;
 
   return children;
 };
