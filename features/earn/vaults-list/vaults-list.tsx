c8ca76e323e5a0deff69173c0ac8664fe4ceab46
Evgeny Taktarov <je.mouth@gmail.com>
2025-08-28 12:44:08 +0200

    feat: disclaimer component

--- features/earn/vaults-list/vaults-list.tsx
+++ features/earn/vaults-list/vaults-list.tsx
@@ -3,8 +3,9 @@ import type { FC } from 'react';
 import { VaultCardGGV } from '../vault-ggv';
 import { VaultCardDVV } from '../vault-dvv';
 
-import { VaultsListWrapper, VaultListDisclaimer } from './styles';
+import { VaultsListWrapper } from './styles';
 import { useVaultConfig } from '../shared/hooks/use-vault-config';
+import { VaultDisclaimer } from '../shared/vault-disclaimer';
 
 const VAULT_CARDS = {
   ggv: VaultCardGGV,
@@ -21,13 +22,7 @@ export const EarnVaultsList: FC = () => {
           return <VaultCard key={vault.name} />;
         })}
       </VaultsListWrapper>
-      <VaultListDisclaimer>
-        * Please note that APR/APY figures are only estimates and subject to
-        change at any time. Past performance is not a guarantee of future
-        results. Rewards are influenced by factors outside the platform’s
-        control, including changes to blockchain protocols and validator
-        performance.
-      </VaultListDisclaimer>
+      <VaultDisclaimer />
     </>
   );
 };
