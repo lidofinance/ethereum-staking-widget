96fb9c98b00a53d5db48c34634cd325ce5089809
Alexander Khramov <alx.khramov@gmail.com>
2025-09-20 13:02:30 +0300

    feat: upd stg assets
    Signed-off-by: Alexander Khramov <alx.khramov@gmail.com>

--- features/earn/shared/vault-partners/vault-partners.tsx
+++ features/earn/shared/vault-partners/vault-partners.tsx
@@ -6,18 +6,24 @@ import {
   VaultPartnerText,
 } from './styles';
 import { VaultPartnerType } from '../types';
+import { useThemeToggle } from '@lidofinance/lido-ui';
 
 type VaultPartnersProps = {
   partners?: VaultPartnerType[];
 };
 
 export const VaultPartners: FC<VaultPartnersProps> = ({ partners }) => {
+  const { themeName } = useThemeToggle();
+  const isDarkTheme = themeName === 'dark';
+
   return (
     <VaultPartnersWrapper data-testid="vault-partners">
       {partners?.map((partner, index) => (
         <VaultPartner key={index}>
           <VaultPartnerRole>{partner.role}</VaultPartnerRole>
-          {partner.icon}
+          {partner.iconDarkTheme && isDarkTheme
+            ? partner.iconDarkTheme
+            : partner.icon}
           <VaultPartnerText>{partner.text}</VaultPartnerText>
         </VaultPartner>
       ))}
