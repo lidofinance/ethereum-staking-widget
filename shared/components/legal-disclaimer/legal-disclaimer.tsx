f3413dd2d153cf287149db4f6f3e51432a9d9848
Alexander Khramov <alx.khramov@gmail.com>
2025-11-26 19:16:32 +0300

    feat: update legal disclaimer to include a link to the privacy notice
    Signed-off-by: Alexander Khramov <alx.khramov@gmail.com>

--- shared/components/legal-disclaimer/legal-disclaimer.tsx
+++ shared/components/legal-disclaimer/legal-disclaimer.tsx
@@ -1,9 +1,13 @@
+import { Link } from '@lidofinance/lido-ui';
+import { config } from 'config';
+
 export const LegalDisclaimer = () => (
   <div data-testid="legal-disclaimer">
     <p>
       Your privacy matters. We use cookieless analytics and collect only
       anonymized data for improvements. Cookies are used for functionality only.
-      For more info read Privacy Notice.
+      For more info read{' '}
+      <Link href={`${config.rootOrigin}/privacy-notice`}>Privacy Notice</Link>.
     </p>
   </div>
 );
