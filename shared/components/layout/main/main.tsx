081d280771c48caf57038c81c740f5ec5eb7f1cb
DiRaiks <t.fane@mail.ru>
2025-12-18 16:26:19 +0300

    feat: enhance holiday decorations logic for earn vault page

--- shared/components/layout/main/main.tsx
+++ shared/components/layout/main/main.tsx
@@ -1,18 +1,24 @@
 import { FC } from 'react';
 import { ContainerProps } from '@lidofinance/lido-ui';
 import { useConfig } from 'config';
+import { useRouter } from 'next/router';
 
 import { MainStyle } from './styles';
+import { EARN_PATH } from 'consts/urls';
 
 export const Main: FC<ContainerProps> = (props) => {
   const { size = 'tight', ...rest } = props;
   const { featureFlags } = useConfig().externalConfig;
+  const router = useRouter();
+  // Needed only for holiday decor to be displayed correctly on earn page (holidayDecorEnabled)
+  const isEarnVault = router.pathname.includes(`${EARN_PATH}/[vault]/[action]`);
 
   return (
     <MainStyle
       size={size}
       forwardedAs="main"
       isHolidayDecorEnabled={featureFlags.holidayDecorEnabled}
+      isEarnVault={isEarnVault}
       {...rest}
     />
   );
--- shared/components/layout/main/styles.tsx
+++ shared/components/layout/main/styles.tsx
@@ -3,14 +3,14 @@ import styled from 'styled-components';
 import { devicesHeaderMedia } from 'styles/global';
 
 export const MainStyle = styled(Container)<
-  ContainerProps & { isHolidayDecorEnabled?: boolean }
+  ContainerProps & { isHolidayDecorEnabled?: boolean; isEarnVault?: boolean }
 >`
   position: relative;
   padding-top: ${({ theme }) => theme.spaceMap.sm}px;
   padding-bottom: ${({ theme }) => theme.spaceMap.sm}px;
 
   @media ${devicesHeaderMedia.mobile} {
-    margin-top: ${({ isHolidayDecorEnabled = false }) =>
-      isHolidayDecorEnabled ? '38px' : 'unset'};
+    margin-top: ${({ isHolidayDecorEnabled = false, isEarnVault = false }) =>
+      isHolidayDecorEnabled ? (isEarnVault ? '88px' : '48px') : 'unset'};
   }
 `;
