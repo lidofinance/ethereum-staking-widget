ce634448672164024e3c3f6218e140dfb13f2a31
jake <jake@lido.fi>
2026-04-03 11:16:10 +0300

    fix: refactor of the header tests

--- test/headers.spec.ts
+++ test/headers.spec.ts
@@ -14,12 +14,16 @@ const configPages = CACHE_CONTROL_PAGES;
 configPages[CACHE_CONTROL_PAGES.indexOf('/favicon:size*')] = '/favicon.ico';
 
 test.describe('Page Headers', () => {
+  const isPreview = !CONFIG.STAND_URL?.includes('branch-preview');
+
   test('Config should have all static pages', async () => {
     test.skip(!!CONFIG.STAND_TYPE, 'We cannot access files on stands');
     const pageRoutes = getAllPagesRoutes();
-    pageRoutes.forEach((foundPage) =>
-      expect(CACHE_CONTROL_PAGES.includes(foundPage)).toBe(true),
-    );
+    expect(pageRoutes.length).toBeGreaterThan(0);
+
+    pageRoutes.forEach((foundPage) => {
+      expect(CACHE_CONTROL_PAGES.includes(foundPage)).toBe(true);
+    });
   });
 
   CACHE_CONTROL_PAGES.map((route) =>
@@ -28,24 +32,25 @@ test.describe('Page Headers', () => {
       expect(resp.status()).toBe(200);
       const headers = resp.headers();
 
-      expect.soft(headers['cache-control']).toBe(CACHE_CONTROL_VALUE);
-      expect.soft(headers['referrer-policy']).toBe('same-origin');
-      expect.soft(headers['x-content-type-options']).toBe('nosniff');
-      expect.soft(headers['x-xss-protection']).toBe('1');
-      expect.soft(headers['x-dns-prefetch-control']).toBe('on');
-      expect.soft(headers['x-download-options']).toBe('noopen');
+      await test.step('Check defined headers', async () => {
+        expect.soft(headers['cache-control']).toBe(CACHE_CONTROL_VALUE);
+        expect.soft(headers['referrer-policy']).toBe('same-origin');
+        expect.soft(headers['x-content-type-options']).toBe('nosniff');
+        expect.soft(headers['x-xss-protection']).toBe('1');
+        expect.soft(headers['x-dns-prefetch-control']).toBe('on');
+        expect.soft(headers['x-download-options']).toBe('noopen');
 
-      // except "/manifest.json", "/favicon:size*", "/runtime/window-env.js" urls and preview-stand deploying
-      if (
-        WIDGET_PAGES.includes(route) &&
-        !CONFIG.STAND_URL?.includes('branch-preview')
-      )
-        expect
-          .soft(headers['content-security-policy'])
-          .toContain('default-src');
+        // except "/manifest.json", "/favicon:size*", "/runtime/window-env.js" urls and preview-stand deploying
+        if (WIDGET_PAGES.includes(route) && isPreview)
+          expect
+            .soft(headers['content-security-policy'])
+            .toContain('default-src');
+      });
 
-      expect.soft(headers['x-frame-options']).toBeUndefined();
-      expect.soft(headers[CACHE_CONTROL_HEADER]).toBeUndefined();
+      await test.step('Check undefined headers', async () => {
+        expect.soft(headers['x-frame-options']).toBeUndefined();
+        expect.soft(headers[CACHE_CONTROL_HEADER]).toBeUndefined();
+      });
     }),
   );
 });
