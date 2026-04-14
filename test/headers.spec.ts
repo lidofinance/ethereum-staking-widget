870518c9fc41b1b7295391fe8c397b9d93cb1c43
Evgeny Taktarov <je.mouth@gmail.com>
2026-04-14 17:22:06 +0200

    test: adjust tests

--- test/headers.spec.ts
+++ test/headers.spec.ts
@@ -20,36 +20,6 @@ test.describe('Page Headers', () => {
       expect(resp.status()).toBe(200);
       const headers = resp.headers();
 
-      const reportingURI = headers['reporting-endpoints'];
-      let reportExtra = '';
-      if (reportingURI) {
-        expect(reportingURI).toMatch(/csp-endpoint="[^"]+"/);
-        reportExtra = `; report-to=csp-endpoint`;
-      }
-
-      expect(headers['cache-control']).toBe(CACHE_CONTROL_VALUE);
-
-      expect(headers['referrer-policy']).toBe('same-origin');
-      expect(headers['x-content-type-options']).toBe('nosniff');
-      expect(headers['x-xss-protection']).toBe('1');
-      expect(headers['x-dns-prefetch-control']).toBe('on');
-      expect(headers['x-download-options']).toBe('noopen');
-      expect(headers['x-permitted-cross-domain-policies']).toBe('none');
-      expect(headers['cross-origin-embedder-policy']).toBe(
-        `credentialless${reportExtra}`,
-      );
-      expect(headers['cross-origin-opener-policy']).toBe(
-        `same-origin${reportExtra}`,
-      );
-      expect(headers['permissions-policy']).toBe(
-        'camera=(), microphone=(), geolocation=(), payment=(), accelerometer=(), gyroscope=(), magnetometer=(), display-capture=(), encrypted-media=(), serial=(), xr-spatial-tracking=(), browsing-topics=(), usb=(self), bluetooth=(self), hid=(self), autoplay=(self), fullscreen=(self), picture-in-picture=(self)',
-      );
-
-      expect(headers['reporting-endpoints']).toMatch(/csp-endpoint="[^"]+"/);
-      expect(headers['strict-transport-security']).toBe(
-        'max-age=63072000; includeSubDomains; preload',
-      );
-
       await test.step('Check defined headers', async () => {
         expect.soft(headers['cache-control']).toBe(CACHE_CONTROL_VALUE);
         expect.soft(headers['referrer-policy']).toBe('same-origin');
@@ -57,6 +27,16 @@ test.describe('Page Headers', () => {
         expect.soft(headers['x-xss-protection']).toBe('1');
         expect.soft(headers['x-dns-prefetch-control']).toBe('on');
         expect.soft(headers['x-download-options']).toBe('noopen');
+        expect
+          .soft(headers['strict-transport-security'])
+          .toBe('max-age=63072000; includeSubDomains; preload');
+        expect.soft(headers['x-permitted-cross-domain-policies']).toBe('none');
+        expect.soft(headers['cross-origin-opener-policy']).toBe(`same-origin`);
+        expect
+          .soft(headers['permissions-policy'])
+          .toBe(
+            'camera=(), microphone=(), geolocation=(), payment=(), accelerometer=(), gyroscope=(), magnetometer=(), display-capture=(), encrypted-media=(), serial=(), xr-spatial-tracking=(), browsing-topics=(), usb=(self), bluetooth=(self), hid=(self), autoplay=(self), fullscreen=(self), picture-in-picture=(self)',
+          );
 
         // except "/manifest.json", "/favicon:size*", "/runtime/window-env.js" urls and preview-stand deploying
         if (WIDGET_PAGES.includes(route) && isPreview)
