ab059d620b3516e5c7106a325a83b5ce09013c56
Alexander Khramov <alx.khramov@gmail.com>
2026-06-16 17:12:51 +0300

    fix(earn): gap syntax for VaultFormSection
    Signed-off-by: Alexander Khramov <alx.khramov@gmail.com>

--- features/earn/shared/vault-form-section.ts
+++ features/earn/shared/vault-form-section.ts
@@ -3,5 +3,5 @@ import styled from 'styled-components';
 export const VaultFormSection = styled.div`
   display: flex;
   flex-direction: column;
-  gap: ${({ theme }) => theme.spaceMap.xs};
+  gap: ${({ theme }) => theme.spaceMap.xs}px;
 `;
